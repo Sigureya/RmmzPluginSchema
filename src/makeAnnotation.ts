627d8d7e74ef397471afa3de49451589409a37a0
Sigureya <sigureya1208@gmail.com>
2025-02-21 18:33:16 +0900

    アノテーションの文字列化を修正

--- src/makeAnnotation.ts
+++ src/makeAnnotation.ts
@@ -1,7 +1,21 @@
-import type { AnnotationBase } from "./schema";
+import type {
+  AnnotationBase,
+  BooleanAnnotation,
+  Primitive,
+  Primitive_Numbers,
+  Primitive_Strings,
+} from "./schema";
 
 const xxx2 = () => {};
 
 const xxx = (annotation: AnnotationBase, key: keyof AnnotationBase) => {
   return `@${key} ${annotation[key]}` as const;
 };
+
+const annotationDefaultObject = <T extends object>(annotation: {
+  default: T;
+}) => {};
+
+const makeDefaultPrimitive = (
+  annotation: BooleanAnnotation | Primitive_Numbers | Primitive_Strings
+) => {};
