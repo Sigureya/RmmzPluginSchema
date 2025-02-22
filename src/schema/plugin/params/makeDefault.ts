dde7c30fa0399aafd507be0cb37d25e2fcbad7a6
Sigureya <sigureya1208@gmail.com>
2025-02-22 09:11:57 +0900

    無限再帰防止を追加。今のところは仮の仕様

--- src/schema/plugin/params/makeDefault.ts
+++ src/schema/plugin/params/makeDefault.ts
@@ -6,11 +6,15 @@ export const makeDefault = <T extends Types.AnnotationTypes>(
   return makeDefaultHelper(ant);
 };
 
-const makeDefaultHelper = (ant: Types.AnnotationTypes) => {
+const makeDefaultHelper = (ant: Types.AnnotationTypes, depth = 0) => {
+  if (depth > 32) {
+    throw new Error("Max depth exceeded");
+  }
+
   if (ant.type === "struct") {
     type Result = Record<string, unknown>;
     return Object.entries(ant.struct.params).reduce<Result>((prev, v) => {
-      prev[v[0]] = makeDefaultHelper(v[1] as Types.AnnotationTypes);
+      prev[v[0]] = makeDefaultHelper(v[1] as Types.AnnotationTypes, depth + 1);
       return prev;
     }, {});
   }
