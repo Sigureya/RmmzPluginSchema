a856ca7d121a5985e02fc232e6d11c763e713cc1
Sigureya <sigureya1208@gmail.com>
2025-02-22 23:18:50 +0900

    @paramの生成用関数

--- src/schema/plugin/params/makeParam.ts
+++ src/schema/plugin/params/makeParam.ts
@@ -1,14 +1,39 @@
-import { makeDefault, makeDefaultJSON } from "./makeDefault";
-import { baseAnnotion, typeAnnotation } from "./makeAnnotation";
-import type { AnnotationTypes } from "./types";
+import { makeDefaultJSON } from "./makeDefault";
+import {
+  baseAnnotion,
+  booleanArgAnnotations,
+  comboAnnotations,
+  fileAnnotations,
+  numberArgAnnotations,
+  selectAnnotations,
+  typeAnnotation,
+} from "./makeAnnotation";
+import type { AnnotationTypes, ParamTexts } from "./types";
+import { mapping } from "./mapping";
 
-const xxx = (
+export const uniqueAnnotations = (ant: AnnotationTypes) => {
+  return mapping<`@${string} ${string}`[]>(ant, {
+    boolean: (b) => booleanArgAnnotations(b),
+    number: (num) => numberArgAnnotations(num),
+    file: (f) => fileAnnotations(f),
+    string: () => [],
+    struct: () => [],
+    select: (s) => selectAnnotations(s),
+    combo: (c) => comboAnnotations(c),
+    dataIndex: () => [],
+  });
+};
+
+export const makeParam = (
   name: string,
   ant: AnnotationTypes,
   mode: "@param" | "@arg" = "@param"
-) => {
-  const xxx = `${mode} ${name}` as const;
-  const type = typeAnnotation(ant);
-  const default_ = makeDefaultJSON(ant);
-  const base = baseAnnotion(ant);
+): ParamTexts => {
+  return {
+    other: uniqueAnnotations(ant),
+    default: `@default ${makeDefaultJSON(ant)}`,
+    name: `${mode} ${name}`,
+    type: typeAnnotation(ant),
+    base: baseAnnotion(ant),
+  };
 };
