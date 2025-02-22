0bd2cfdddaf42bd9c807734401a90be90b9989f8
Sigureya <sigureya1208@gmail.com>
2025-02-22 10:13:04 +0900

    テストコードを修正、本体コードの関数を別ファイルに移したのでスリムになった

---
+++ src/schema/plugin/params/makeDefault.test.ts
@@ -0,0 +1,104 @@
+import { test, expect, describe } from "vitest";
+import type { NumberArg, Primitive_NumbersArray } from "./types/primitive";
+import { makeDefault } from "./makeDefault";
+import type { Struct, Type_Struct } from "./types";
+
+const mockNumber: NumberArg = {
+  default: 123,
+  type: "number",
+};
+
+const mockNumberArray: Primitive_NumbersArray = {
+  default: [1, 2, 3],
+  type: "number[]",
+};
+
+describe("makeDefault", () => {
+  test("number", () => {
+    const result: number = makeDefault(mockNumber);
+    expect(result).toBe(123);
+  });
+  test("numberArray", () => {
+    const result: number[] = makeDefault(mockNumberArray);
+    expect(result).toEqual([1, 2, 3]);
+  });
+});
+interface Parson {
+  name: string;
+  age: number;
+}
+interface Home {
+  name: string;
+  address: {
+    street: string;
+    city: string;
+  };
+  family: Parson[];
+}
+
+const mockParson: Type_Struct<Parson> = {
+  type: "struct",
+  struct: {
+    structName: "Parson",
+    params: {
+      name: {
+        type: "string",
+        default: "John",
+      },
+      age: {
+        type: "number",
+        default: 30,
+      },
+    },
+  },
+};
+
+const mockHome: Type_Struct<Home> = {
+  type: "struct",
+  struct: {
+    structName: "Home",
+    params: {
+      name: {
+        type: "string",
+        default: "Home",
+      },
+      address: {
+        type: "struct",
+        struct: {
+          structName: "Address",
+          params: {
+            street: {
+              type: "string",
+              default: "sss",
+            },
+            city: {
+              type: "string",
+              default: "ccc",
+            },
+          },
+        },
+      },
+      family: {
+        type: "struct[]",
+        struct: mockParson.struct,
+        default: [],
+      },
+    },
+  },
+};
+describe("makeDefault struct", () => {
+  test("parson", () => {
+    const parson: Parson = makeDefault(mockParson);
+    const expected: Parson = { name: "John", age: 30 };
+    expect(parson).toEqual(expected);
+  });
+  test("home", () => {
+    const home = makeDefault(mockHome);
+    const expected: Home = {
+      name: "Home",
+      address: { street: "sss", city: "ccc" },
+      family: [],
+    };
+    expect(home).toEqual(expected);
+  });
+});
