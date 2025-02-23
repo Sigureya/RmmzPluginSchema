a7c43adedca771bf000b1306d89a1eec30001766
Sigureya <sigureya1208@gmail.com>
2025-02-24 01:21:03 +0900

    ESLintの設定を修正

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,7 @@ export default [
     },
     rules: {
       "@typescript-eslint/explicit-function-return-type": "off",
-      //      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/no-explicit-any": "warn",
       "@typescript-eslint/consistent-type-imports": [
         "warn",
