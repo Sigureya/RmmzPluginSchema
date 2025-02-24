427416355ceb103139939ee8753974a29433a949
Sigureya <sigureya1208@gmail.com>
2025-02-25 01:34:11 +0900

    辞書を使って文字列を差し替える機能。検証すべき箇所が多数あるが、ひとまずはファイルだけ対応させる

--- packages/schema/src/plugin/params/makeDefault.ts
+++ packages/schema/src/plugin/params/makeDefault.ts
@@ -1,29 +1,53 @@
+import { dicEX } from "./makeAnnotation";
 import type * as Types from "./types/";
 
-export const makeDefaultValue = <T extends Types.AnnotationTypes>(
-  ant: T
+// const isNotTextType = (annotation: Types.AnnotationTypes) => {
+//   return annotation.type === "file" || annotation.type === "";
+// };
+
+// selectは差し替えない。おそらくenum的に使われており、テキストではない
+// comboは差し替える。まあテキストでしょう
+// fileは差し替えない
+
+export const makeDefaultValue = (
+  annotation: Types.AnnotationTypes,
+  dic: Types.Dictionary = {}
 ): string => {
-  const value = makeDefault(ant);
-  return typeof value === "string" ? value : JSON.stringify(value, null, 0);
+  // ファイル名はそのまま。変換テーブルで差し替えると危険なので
+  if (annotation.type === "file") {
+    return makeDefault(annotation);
+  }
+  const value = makeDefault(annotation);
+  return typeof value === "string"
+    ? dicEX(value, dic)
+    : JSON.stringify(value, null, 0);
 };
 
 export const makeDefault = <T extends Types.AnnotationTypes>(
-  ant: T
+  annotation: T
 ): NonNullable<T["default"]> => {
-  return ant.default === undefined ? makeDefaultHelper(ant) : ant.default;
+  return annotation.default === undefined
+    ? makeDefaultHelper(annotation)
+    : annotation.default;
 };
 
-const makeDefaultHelper = (ant: Types.AnnotationTypes, depth = 0) => {
+const makeDefaultHelper = (annotation: Types.AnnotationTypes, depth = 0) => {
   if (depth > 32) {
     throw new Error("Max depth exceeded");
   }
 
-  if (ant.type === "struct") {
+  if (annotation.type === "struct") {
     type Result = Record<string, unknown>;
-    return Object.entries(ant.struct.params).reduce<Result>((prev, v) => {
-      prev[v[0]] = makeDefaultHelper(v[1] as Types.AnnotationTypes, depth + 1);
-      return prev;
-    }, {});
+    return Object.entries(annotation.struct.params).reduce<Result>(
+      (prev, v) => {
+        prev[v[0]] = makeDefaultHelper(
+          v[1] as Types.AnnotationTypes,
+          depth + 1
+        );
+        return prev;
+      },
+      {}
+    );
   }
-  return ant.default;
+  return annotation.default;
 };
