9933578e0953c0f42cb2151eb49e5adbed1f6042
Sigureya <sigureya1208@gmail.com>
2025-02-24 18:07:48 +0900

    とりあえずビルド。若干マシなパスになった

--- packages/vitePlugins/dist/rmmzAnnotation.d.ts
+++ packages/vitePlugins/dist/rmmzAnnotation.d.ts
@@ -1,3 +1,3 @@
 import { Plugin } from 'vite';
-import { Struct } from '../packages/schema/src/index.ts';
+import { Struct } from '../../schema/src';
 export declare const rmmzAnnotation: (s: Struct<object>[]) => Plugin;
