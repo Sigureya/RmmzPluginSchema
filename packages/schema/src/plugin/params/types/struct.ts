8f7f015563f34d3ac5059e72e42701782b632090
Sigureya <sigureya1208@gmail.com>
2025-02-26 00:32:25 +0900

    structの新型を暫定的に適用。関数の引数で使う型と、オブジェクト構築に使う型の両方を用意していく

--- packages/schema/src/plugin/params/types/struct.ts
+++ packages/schema/src/plugin/params/types/struct.ts
@@ -1,3 +1,4 @@
+import type { StructNode_Error } from "./errors";
 import type {
   AnnotationBase,
   Primitive_Numbers,
@@ -15,6 +16,28 @@ export interface StructBase {
 }
 export interface ParameterBase extends Record<string, AnnotationTypes> {}
 
+type StructNodeType =
+  | StructBaseVirtual
+  | StructBaseUnnamed
+  | StructBaseXX<object>;
+// 新規追加。特定のオブジェクトのメンバだけで使うような型を簡単に書くためのパターン
+export interface StructBaseUnnamed {
+  structName?: string;
+  params: ParameterBase;
+}
+
+// 新規。名前だけ判明しており、どこかに定義があるパターン
+export interface StructBaseVirtual {
+  structName: string;
+  params?: ParameterBase;
+}
+
+// 初期値だけ判明しているが、型情報が無いパターン。自動生成することになる。
+// これを再帰すると地獄になるので、valueはある程度制限する
+export interface StructBaseXX<T extends object> {
+  default: T;
+}
+
 export interface Struct<T extends object> extends StructBase {
   structName: string;
   params: StructParameters<T>;
@@ -29,14 +52,18 @@ export type ParamType<T> = T extends number | string | boolean
   : T extends number[] | string[]
   ? PrimitiveArray<T>
   : T extends object[]
-  ? Type_StructArray<T, Struct<T[number]>>
+  ? Type_StructArray<T>
   : T extends object
-  ? Type_Struct<T, Struct<T>>
-  : never;
+  ? Type_Struct<T>
+  : StructNode_Error<"unknown">;
 
 export type AnnotationTypes =
   | Type_StructArray<object[]>
   | Type_Struct<object>
+  | AnnotationPrimitiveTypes
+  | StructNode_Error;
+
+export type AnnotationPrimitiveTypes =
   | BooleanArg
   | Primitive_Numbers
   | Primitive_NumbersArray
@@ -48,20 +75,14 @@ export interface HasStruct extends Omit<AnnotationBase, "default"> {
   default?: unknown;
 }
 
-export interface Type_StructArray<
-  Array extends object[],
-  ArrayAnnotation extends StructBase = Struct<Array[number]>
-> extends HasStruct {
+export interface Type_StructArray<Array extends object[]> extends HasStruct {
   type: "struct[]";
-  struct: ArrayAnnotation;
+  struct: Struct<Array[number]>;
   default: Array;
 }
 
-export interface Type_Struct<
-  T extends object,
-  StructAnnotation extends StructBase = Struct<T>
-> extends HasStruct {
+export interface Type_Struct<T extends object> extends HasStruct {
   type: "struct";
-  struct: StructAnnotation;
+  struct: Struct<T>;
   default?: T;
 }
---
+++ packages/schema/src/plugin/params/types/struct2.ts
@@ -0,0 +1,221 @@
+import type { StructNode_Error } from "./errors";
+import type {
+  AnnotationBase,
+  BooleanArg,
+  Primitive,
+  Primitive_Numbers,
+  Primitive_NumbersArray,
+  Primitive_Strings,
+  Primitive_StringsArray,
+  PrimitiveArray,
+} from "./primitive";
+import type { AnnotationTypes } from "./struct";
+
+interface StructRoot<T extends object> extends StructType2<T, T, "root"> {}
+
+type StructUnion =
+  | NodeItem_Array<object[], object, string>
+  | NodeItem_Struct<object, object, string>
+  | NodeItem_TypelessStruct<object>;
+
+interface StructName {
+  structName: string;
+  params?: ParameterBase2;
+}
+
+interface StructBase2 {
+  structName: string;
+  params: ParameterBase2;
+}
+
+interface ParameterBase2
+  extends Record<string, AnnotationTypes | StructUnion> {}
+
+interface StructType2<
+  T extends object,
+  KnowTypes extends object,
+  Path extends string = ""
+> {
+  structName: string;
+  params: StructParameters2<T, KnowTypes, Path>;
+}
+type StructParameters2<
+  T extends object,
+  KnowTypes extends object,
+  Path extends string = ""
+> = {
+  [Key in Extract<keyof T, string>]: ParamType2<
+    T[Key],
+    KnowTypes,
+    `${Path}.${Key}`
+  >;
+};
+export type AnnotationTypes2 =
+  | BooleanArg
+  | Primitive_Numbers
+  | Primitive_NumbersArray
+  | Primitive_Strings
+  | Primitive_StringsArray
+  | StructNode_Error;
+
+export type ParamType2<
+  T,
+  KnowTypes extends object,
+  Path extends string = "?"
+> = T extends number | string | boolean
+  ? Primitive<T>
+  : T extends number[] | string[]
+  ? PrimitiveArray<T>
+  : T extends KnowTypes
+  ? StructNode_Error<Path>
+  : T extends KnowTypes[]
+  ? StructNode_Error<`${Path}[]`>
+  : T extends object[]
+  ? NodeItem_Array<T, KnowTypes, Path>
+  : T extends object
+  ? NodeItem_Struct<T, KnowTypes, Path> | NodeItem_TypelessStruct<T>
+  : StructNode_Error<`never:${Path}`>;
+
+interface HasStruct2 {
+  struct: StructBase2;
+  default?: unknown;
+}
+export interface NodeItem_Array<
+  Array extends object[],
+  KnowTypes extends object,
+  Path extends string = "array?"
+> extends HasStruct2 {
+  type: "struct[]";
+  struct: StructType2<Array[number], KnowTypes | Array[number], Path>;
+  default: Array;
+}
+
+export interface NodeItem_Struct<
+  T extends object,
+  KnowTypes extends object,
+  Path extends string = "struct?"
+> extends HasStruct2 {
+  type: "struct";
+  struct: StructType2<T, KnowTypes, Path>;
+  default?: T;
+}
+
+export type StructUnion2<T extends object> =
+  | StructWithName<T>
+  | StructWithParams<T>
+  | StructWithDefault<T>;
+
+export interface BaseStruct<T extends object>
+  extends Omit<AnnotationBase, "default"> {
+  type: "struct";
+  struct?: {
+    structName?: string;
+    params?: ParameterBase2;
+  };
+  default?: T;
+}
+
+export interface StructComplete<T extends object> extends BaseStruct<T> {
+  type: "struct";
+  struct: {
+    structName: string;
+    params: ParameterBase2;
+  };
+  default: T;
+}
+
+interface StructWithName<T extends object> extends BaseStruct<T> {
+  struct: {
+    structName: string;
+    params?: ParameterBase2;
+  };
+  default?: T;
+}
+
+export interface StructWithParams<T extends object> extends BaseStruct<T> {
+  struct: {
+    structName?: string;
+    params: ParameterBase2;
+  };
+  default?: T;
+}
+interface StructWithDefault<T extends object> extends BaseStruct<T> {
+  default: T;
+}
+
+export interface NodeItem_TypelessStruct<T extends object = object> {
+  type: "struct";
+  default: T;
+}
+
+interface Parson {
+  name: string;
+  age: number;
+}
+interface Home {
+  name: string;
+  address: {
+    street: string;
+    city: string;
+  };
+  family: Parson[];
+}
+
+const pp: StructRoot<Parson> = {
+  structName: "Parson",
+
+  params: {
+    name: {
+      type: "string",
+      default: "John",
+    },
+    age: {
+      type: "number",
+      default: 30,
+    },
+  },
+};
+
+const xx: StructRoot<Home> = {
+  structName: "Home",
+
+  params: {
+    name: {
+      type: "string",
+      default: "John",
+    },
+    family: {
+      type: "struct[]",
+      struct: {
+        structName: "Parson",
+        params: {
+          name: {
+            type: "string",
+            default: "John",
+          },
+          age: {
+            type: "number",
+            default: 30,
+          },
+        },
+      },
+      default: [],
+    },
+    address: {
+      type: "struct",
+      struct: {
+        structName: "Address",
+        params: {
+          street: {
+            type: "string",
+            default: "sss",
+          },
+          city: {
+            type: "string",
+            default: "ccc",
+          },
+        },
+      },
+    },
+  },
+};
