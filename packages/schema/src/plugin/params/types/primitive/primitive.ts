f09bfa0591e1702889c3e15091b67e39e086ac5e
Sigureya <sigureya1208@gmail.com>
2025-02-27 22:28:30 +0900

    typeの段階を修正。配列型と単独型を分けて扱う必要が出てきた

--- packages/schema/src/plugin/params/types/primitive/primitive.ts
+++ packages/schema/src/plugin/params/types/primitive/primitive.ts
@@ -2,9 +2,15 @@ import type { BooleanArg } from "./boolean";
 import type { Primitive_Numbers, Primitive_NumbersArray } from "./numbers";
 import type { Primitive_Strings, Primitive_StringsArray } from "./strings";
 
-export type AnnotationPrimitiveTypes =
+export type AnnotationSigleTypes =
   | BooleanArg
   | Primitive_Numbers
+  | Primitive_Strings;
+
+export type AnnotationArrayTypes =
   | Primitive_NumbersArray
-  | Primitive_Strings
   | Primitive_StringsArray;
+
+export type AnnotationPrimitiveTypes =
+  | AnnotationSigleTypes
+  | AnnotationArrayTypes;
