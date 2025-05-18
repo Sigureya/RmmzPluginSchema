773e6eefbc9d46f720e00139bb76fff03fa70183
Sigureya <sigureya1208@gmail.com>
2025-05-18 19:41:35 +0900

    古いビルド結果を削除

--- packages/schema/dist/plugin/params/makeParam.d.ts
+++
@@ -1,4 +0,0 @@
-import { AnnotationPrimitiveTypes, Dictionary, ParamTexts, StructAnnotationBase_Completed } from './types';
-export declare const uniqueAnnotations: (ant: AnnotationPrimitiveTypes | StructAnnotationBase_Completed, dic?: Dictionary) => `@${string} ${string}`[];
-export declare const makeParam: (name: string, ant: AnnotationPrimitiveTypes | StructAnnotationBase_Completed, mode?: "@param" | "@arg", dic?: Dictionary) => ParamTexts;
-export declare const joinAnntations: (paramTexts: ParamTexts) => string[];
--- packages/schema/dist/plugin/params/types/primitive/primitive.d.ts
+++
@@ -1,4 +0,0 @@
-import { BooleanArg } from './boolean';
-import { Primitive_Numbers, Primitive_NumbersArray } from './numbers';
-import { Primitive_Strings, Primitive_StringsArray } from './strings';
-export type AnnotationPrimitiveTypes = BooleanArg | Primitive_Numbers | Primitive_NumbersArray | Primitive_Strings | Primitive_StringsArray;
