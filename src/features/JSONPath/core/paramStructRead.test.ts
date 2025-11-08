5c759ef8b3b87ea3f3024befcc9c131842754ed0
Sigureya <sigureya1208@gmail.com>
2025-11-08 14:33:13 +0900

    小さめのテストコード。引数と戻り値の具体例として

--- src/features/JSONPath/core/paramStructRead.test.ts
+++ src/features/JSONPath/core/paramStructRead.test.ts
@@ -1,14 +1,198 @@
 import { describe, test, expect } from "vitest";
-import type { JSONValue } from "@RpgTypes/libs";
 import type { PrimitiveParam } from "@RpgTypes/rmmz/plugin";
 import type {
-  NumberSequenceParamValues,
   ScalaPathResult,
   StringSequenceParamValues,
 } from "./arrayEx/types/result";
+import {
+  extractArrayValuesFromJson,
+  extractScalaValuesFromJson,
+} from "./paramStructRead";
 import type { StructPropertysPath } from "./types";
-describe.skip("extractScalaValuesFromJson", () => {
-  test("TODO", () => {
-    expect(true).toBe(true);
+
+interface Person {
+  name: string;
+  age: number;
+  isStudent: boolean;
+  nicknames: string[];
+}
+
+interface MockData {
+  person: Person;
+  students: Person[];
+}
+
+const mockData = {
+  person: {
+    name: "Grad",
+    age: 30,
+    isStudent: false,
+    nicknames: ["G", "Grady", "Big G"],
+  },
+  students: [
+    { name: "Alice", age: 20, isStudent: true, nicknames: ["Ally", "Lice"] },
+    { name: "Bob", age: 22, isStudent: false, nicknames: ["Bobby"] },
+    { name: "Charlie", age: 19, isStudent: true, nicknames: [] },
+  ],
+} as const satisfies MockData;
+
+const personSchema = {
+  name: {
+    kind: "string",
+    default: "",
+    text: "person name",
+  },
+  age: { kind: "number", default: 0 },
+  isStudent: { kind: "boolean", default: false },
+} as const satisfies Record<string, PrimitiveParam>;
+
+describe("extractScalaValuesFromJson", () => {
+  test("should return empty array when scalas is undefined", () => {
+    const path: StructPropertysPath = {
+      structName: "TestStruct",
+      arraySchema: {},
+      scalaArrays: [],
+      objectSchema: {},
+      scalas: undefined,
+    };
+    const reuslt = extractScalaValuesFromJson(mockData, path);
+    expect(reuslt).toEqual([]);
+  });
+  test("should extract scala values from json", () => {
+    const path: StructPropertysPath = {
+      structName: "Person",
+      arraySchema: {},
+      scalaArrays: [],
+      objectSchema: personSchema,
+      scalas: "$.person['name','age','isStudent']",
+    };
+    const expected: ScalaPathResult[] = [
+      {
+        structName: "Person",
+        value: "Grad",
+        param: {
+          name: "name",
+          attr: { kind: "string", default: "", text: "person name" },
+        },
+      },
+      {
+        structName: "Person",
+        value: 30,
+        param: { name: "age", attr: { kind: "number", default: 0 } },
+      },
+      {
+        structName: "Person",
+        value: false,
+        param: { name: "isStudent", attr: { kind: "boolean", default: false } },
+      },
+    ];
+    const result = extractScalaValuesFromJson(mockData, path);
+    expect(result).toEqual(expected);
+  });
+  test("students", () => {
+    const path: StructPropertysPath = {
+      structName: "Person",
+      arraySchema: {},
+      scalaArrays: [],
+      objectSchema: personSchema,
+      scalas: `$.students[*]["name","age"]`,
+    };
+    const expected: ScalaPathResult[] = [
+      {
+        structName: "Person",
+        value: "Alice",
+        param: {
+          name: "name",
+          attr: personSchema.name,
+        },
+      },
+      {
+        structName: "Person",
+        value: 20,
+        param: { name: "age", attr: personSchema.age },
+      },
+      {
+        structName: "Person",
+        value: "Bob",
+        param: {
+          name: "name",
+          attr: personSchema.name,
+        },
+      },
+      {
+        structName: "Person",
+        value: 22,
+        param: { name: "age", attr: personSchema.age },
+      },
+      {
+        structName: "Person",
+        value: "Charlie",
+        param: {
+          name: "name",
+          attr: personSchema.name,
+        },
+      },
+      {
+        structName: "Person",
+        value: 19,
+        param: { name: "age", attr: personSchema.age },
+      },
+    ];
+    const result = extractScalaValuesFromJson(mockData, path);
+    expect(result).toEqual(expected);
+  });
+});
+
+describe("extractArrayValuesFromJson", () => {
+  test("should return empty array when scalaArrays is empty", () => {
+    const path: StructPropertysPath = {
+      structName: "Person",
+      scalas: undefined,
+      objectSchema: personSchema,
+      arraySchema: {},
+      scalaArrays: [],
+    };
+    const result = extractArrayValuesFromJson(mockData, path);
+    expect(result).toEqual([]);
+  });
+  test("should extract array values from json", () => {
+    const path: StructPropertysPath = {
+      structName: "Person",
+      scalas: "dummyPath",
+      objectSchema: personSchema,
+      arraySchema: {},
+      scalaArrays: [],
+    };
+    const result = extractArrayValuesFromJson(mockData, path);
+    expect(result).toEqual([]);
+  });
+  test("should extract array values from json with scalaArrays", () => {
+    const path: StructPropertysPath = {
+      structName: "Person",
+      scalas: undefined,
+      objectSchema: personSchema,
+      arraySchema: {},
+      scalaArrays: [
+        {
+          path: "$.person.nicknames[*]",
+          param: {
+            name: "nicknames",
+            attr: { kind: "string[]", default: [] },
+          },
+        },
+      ],
+    };
+    const expected: StringSequenceParamValues[] = [
+      {
+        values: ["G", "Grady", "Big G"],
+        valueKind: "string",
+        param: {
+          name: "nicknames",
+          attr: { kind: "string[]", default: [] },
+        },
+      },
+    ];
+    const result = extractArrayValuesFromJson(mockData, path);
+    expect(result).toEqual(expected);
   });
 });
