03970da09dd3de5e675d02885c4db50d58c231b9
Sigureya <sigureya1208@gmail.com>
2025-11-08 17:57:27 +0900

    シンプルなパスを用意。struct[]の場合の戻り値が複雑なため、専用テストが必要だった

---
+++ src/features/JSONPath/core/paramStruct.structArray.test.ts
@@ -0,0 +1,104 @@
+import { describe, test, expect } from "vitest";
+import type {
+  ClassifiedPluginParamsEx,
+  ClassifiedPluginParams,
+  PluginParamEx,
+  StructRefParam,
+} from "@RmmzPluginSchema/rmmz/plugin";
+import { getPathFromStructParam } from "./paramStruct";
+import type { StructPathResult, StructPropertysPath } from "./types";
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+interface Item {
+  itemId: number;
+  itemName: string;
+}
+
+interface ClassRoom {
+  students: Person[];
+  items: Item[];
+}
+
+const personSchema: ClassifiedPluginParamsEx<Person> = {
+  scalaArrays: [],
+  structs: [],
+  structArrays: [],
+  scalas: [
+    { name: "name", attr: { kind: "string", default: "" } },
+    { name: "age", attr: { kind: "number", default: 0 } },
+  ],
+};
+
+const classRoomSchema: ClassifiedPluginParamsEx<ClassRoom> = {
+  scalaArrays: [],
+  structs: [],
+  scalas: [],
+  structArrays: [
+    {
+      name: "students",
+      attr: { struct: "Person", kind: "struct[]" },
+    },
+    {
+      name: "items",
+      attr: { struct: "Item", kind: "struct[]" },
+    },
+  ],
+};
+
+const itemSchema: ClassifiedPluginParamsEx<Item> = {
+  scalaArrays: [],
+  structs: [],
+  structArrays: [],
+  scalas: [
+    { name: "itemId", attr: { kind: "number", default: 0 } },
+    { name: "itemName", attr: { kind: "string", default: "" } },
+  ],
+};
+
+const structsMap: ReadonlyMap<string, ClassifiedPluginParams> = new Map<
+  string,
+  ClassifiedPluginParams
+>([
+  ["Person", personSchema],
+  ["ClassRoom", classRoomSchema],
+  ["Item", itemSchema],
+]);
+
+describe("getPathFromStructParam", () => {
+  const path1: StructPropertysPath = {
+    objectSchema: {
+      age: { default: 0, kind: "number" },
+      name: { default: "", kind: "string" },
+    },
+    scalaArrays: [],
+    scalas: '$.classRoom.students[*]["name","age"]',
+    structName: "Person",
+  };
+  const path2: StructPropertysPath = {
+    objectSchema: {
+      itemId: { default: 0, kind: "number" },
+      itemName: { default: "", kind: "string" },
+    },
+    scalaArrays: [],
+    scalas: '$.classRoom.items[*]["itemId","itemName"]',
+    structName: "Item",
+  };
+
+  test("classRoom.students", () => {
+    const params: ReadonlyArray<PluginParamEx<StructRefParam>> = [
+      { name: "classRoom", attr: { kind: "struct", struct: "ClassRoom" } },
+    ];
+    const result: StructPathResult = getPathFromStructParam(
+      params,
+      "$",
+      structsMap
+    );
+    const expected: StructPropertysPath[] = [path1, path2];
+    expect(result.errors).toEqual([]);
+    expect(result.items).toEqual(expected);
+  });
+});
