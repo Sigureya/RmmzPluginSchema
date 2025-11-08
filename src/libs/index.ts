3691dca86469ed4a817daac1a5d0d3d3444a252a
Sigureya <sigureya1208@gmail.com>
2025-11-08 09:02:09 +0900

    souceIdを追加定義。

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -1,3 +1,5 @@
 export * from "./namedItemSource";
+export * from "./sourceIdTypes";
+export * from "./sourceIdTypesRpg";
 export * from "./templates";
 export * from "./utils";
---
+++ src/libs/sourceIdTypesRpg.ts
@@ -0,0 +1,31 @@
+import type { SourceIdentifier } from "./namedItemSource";
+
+export interface SourceId_Data<Kind extends string> extends SourceIdentifier {
+  author: "rmmz";
+  module: "data";
+  kind: Kind;
+}
+
+export type SourceId_DataActor = SourceId_Data<"actor">;
+export type SourceId_DataArmor = SourceId_Data<"armor">;
+export type SourceId_DataClass = SourceId_Data<"class">;
+export type SourceId_DataEnemy = SourceId_Data<"enemy">;
+export type SourceId_DataItem = SourceId_Data<"item">;
+export type SourceId_DataSkill = SourceId_Data<"skill">;
+export type SourceId_DataState = SourceId_Data<"state">;
+export type SourceId_DataWeapon = SourceId_Data<"weapon">;
+export type SourceId_DataTroop = SourceId_Data<"troop">;
+
+export type SourceId_DataCommonEvent = SourceId_Data<"common_event">;
+
+export type SourceIdUnion_RpgData =
+  | SourceId_DataActor
+  | SourceId_DataArmor
+  | SourceId_DataClass
+  | SourceId_DataEnemy
+  | SourceId_DataItem
+  | SourceId_DataSkill
+  | SourceId_DataState
+  | SourceId_DataWeapon
+  | SourceId_DataCommonEvent
+  | SourceId_DataTroop;
