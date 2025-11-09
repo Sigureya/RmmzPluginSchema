a39bfe3bbb5d99676fc55858dab014fb4ade9250
Sigureya <sigureya1208@gmail.com>
2025-11-10 07:41:03 +0900

    structの配列をMap<>に変換する関数。どうやら用途が複数あるので、そこそこに再編していく

---
+++ src/rmmz/plugin/core/params/structMap.ts
@@ -0,0 +1,28 @@
+import { classifyPluginParams } from "./classify";
+import type {
+  ClassifiedPluginParams,
+  PluginStructSchemaArray,
+  PrimitiveParam,
+} from "./types";
+
+export const createStructMapclassifyed = (
+  bundle: ReadonlyArray<PluginStructSchemaArray>
+): Map<string, ClassifiedPluginParams> => {
+  return new Map(
+    bundle.map((s): [string, ClassifiedPluginParams] => [
+      s.struct,
+      classifyPluginParams(s.params),
+    ])
+  );
+};
+
+export const createStructMap = (
+  structs: ReadonlyArray<PluginStructSchemaArray>
+): Map<string, PrimitiveParam[]> => {
+  return new Map(
+    structs.map((s): [string, PrimitiveParam[]] => [
+      s.struct,
+      s.params.map((p) => p.attr),
+    ])
+  );
+};
