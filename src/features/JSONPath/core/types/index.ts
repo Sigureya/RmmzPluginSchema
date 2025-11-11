edcf73bcd1d91e82e544275fa5908ed12a2d2584
Sigureya <sigureya1208@gmail.com>
2025-11-11 12:22:36 +0900

    そのうち廃止する。まずimportだけ切っておく

--- src/features/JSONPath/core/types/index.ts
+++ src/features/JSONPath/core/types/index.ts
@@ -1,5 +1,1 @@
-export * from "../createPath/errorTypes";
-export * from "../createPath/pathSchemaTypes";
-export * from "../createPath/types/types";
-export * from "./memo";
 export * from "./result";
