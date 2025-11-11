c6f74587872fbcd2128ee747265cd224cd0a235a
Sigureya <sigureya1208@gmail.com>
2025-11-11 13:52:14 +0900

    単独の値を扱う時のパス。

--- src/features/JSONPath/core/memo2/types/scalar.ts
+++ src/features/JSONPath/core/memo2/types/scalar.ts
@@ -1,7 +1,15 @@
 import type { PluginParamEx, ScalarParam } from "@RmmzPluginSchema/rmmz/plugin";
 import type { JSONPathJS } from "jsonpath-js";
+import type { PluginValues, ValueCategory2 } from "./result";
 
 export interface StructPropertysMemo {
   jsonPathJS: JSONPathJS;
-  propetys: Record<string, PluginParamEx<ScalarParam>>;
+  propetys: Record<string, ScalarParam>;
+}
+
+export interface PluginValueScalar extends PluginValues {
+  value: number | string | boolean;
+  category: ValueCategory2;
+  name: string;
+  param: PluginParamEx<ScalarParam>;
 }
