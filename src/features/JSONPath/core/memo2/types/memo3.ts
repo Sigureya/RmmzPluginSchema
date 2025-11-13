e607a646afd5ee410715eabc72ad8ccb7c2b4671
Sigureya <sigureya1208@gmail.com>
2025-11-13 19:14:42 +0900

    データの持ち方を変更。あとで戻すかもしれないが、混乱を減らせることが重要

--- src/features/JSONPath/core/memo2/types/memo3.ts
+++ src/features/JSONPath/core/memo2/types/memo3.ts
@@ -2,8 +2,10 @@ import type { JSONPathReader } from "@RmmzPluginSchema/libs/jsonPath";
 import type { ScalarParam } from "@RmmzPluginSchema/rmmz/plugin";
 import type { ArrayPathMemo } from "./array";
 
-export interface PluginValuesPathMemo3 {
-  schema2: Record<string, ScalarParam>;
-  jsonPathJS: JSONPathReader;
+export interface PluginValuesPathMemo4 {
+  scalar?: {
+    jsonPathJS: JSONPathReader;
+    record: Record<string, ScalarParam>;
+  };
   arrays: ArrayPathMemo[];
 }
