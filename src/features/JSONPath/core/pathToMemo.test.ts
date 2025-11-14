d37c7e2946053aad763e113101ecdf8156bf6fe0
Sigureya <sigureya1208@gmail.com>
2025-11-14 15:44:22 +0900

    本体とテストコードを再構築。想像以上にリファクタリングが必要

--- src/features/JSONPath/core/pathToMemo.test.ts
+++ src/features/JSONPath/core/pathToMemo.test.ts
@@ -1,4 +1,5 @@
-import { describe, test, expect } from "vitest";
+import type { MockedFunction } from "vitest";
+import { describe, test, expect, vi } from "vitest";
 import type {
   ClassifiedPluginParamsEx,
   ClassifiedPluginParams,
@@ -6,11 +7,12 @@ import type {
   StructRefParam,
 } from "@RmmzPluginSchema/rmmz/plugin";
 import { toObjectPluginParams } from "@RmmzPluginSchema/rmmz/plugin";
-import { getPathFromStructParam } from "./createPath/structValue";
-import type { StructPathResult, StructPropertysPath } from "./createPath/types";
-import { memo3 } from "./memo2/memo3";
+import { JSONPathJS } from "jsonpath-js";
+import type { PluginValuesPathWithError } from "./createPath/types";
+import { createPluginValuesPathPP } from "./createPath/valuePath";
+import { runMemoBundle } from "./memo2/memo3";
 import type { PluginValues } from "./memo2/types";
-import type { PluginValuesPathMemo4 } from "./memo2/types/memo3";
+import type { MemoBundle } from "./memo2/types/memo3";
 import { createMemoFromPath } from "./pathToMemo";
 
 interface Person {
@@ -99,18 +101,19 @@ const makeMap = (): ReadonlyMap<string, ClassifiedPluginParams> => {
   ]);
 };
 
+const createMockFunc = (): MockedFunction<(path: string) => JSONPathJS> => {
+  return vi.fn(newJSONPath);
+};
+
+const newJSONPath = (path: string): JSONPathJS => {
+  return new JSONPathJS(path);
+};
+
 describe("address", () => {
-  const path: StructPropertysPath = {
-    category: "struct",
-    name: "Address",
-    scalars: `$.address["street","city","zipCode"]`,
-    scalarArrays: [],
-    objectSchema: toObjectPluginParams(addressSchema.scalars),
-  };
-  const paramSchema = {
+  const paramSchema: PluginParamEx<StructRefParam> = {
     name: "address",
     attr: { kind: "struct", struct: "Address" },
-  } as const satisfies PluginParamEx<StructRefParam>;
+  };
   const paramObject = {
     address: {
       city: "Sample City",
@@ -118,11 +121,55 @@ describe("address", () => {
       zipCode: "12345",
     } as const satisfies Address,
   };
-  test("getPathFromStruct", () => {
-    const structMap = makeMap();
-    const result = getPathFromStructParam([paramSchema], "$", structMap);
-    expect(result.items).toEqual([path]);
-    expect(result.errors).toEqual([]);
+
+  const pathV2: PluginValuesPathWithError = {
+    scalars: {
+      category: "param",
+      name: "address",
+      objectSchema: {},
+      scalars: undefined,
+      scalarArrays: [],
+    },
+    structArrays: { items: [], errors: [] },
+    structs: {
+      items: [
+        {
+          category: "struct",
+          name: "Address",
+          scalars: `$.address["street","city","zipCode"]`,
+          scalarArrays: [],
+          objectSchema: toObjectPluginParams(addressSchema.scalars),
+        },
+      ],
+      errors: [],
+    },
+  };
+
+  test("p2", () => {
+    const result: PluginValuesPathWithError = createPluginValuesPathPP(
+      "param",
+      paramSchema,
+      makeMap()
+    );
+    expect(result.structArrays).toEqual(pathV2.structArrays);
+    expect(result.scalars).toEqual(pathV2.scalars);
+    expect(result.structs).toEqual(pathV2.structs);
+    expect(result).toEqual(pathV2);
+  });
+
+  test("calls jsonPath factory", () => {
+    const mockFn = createMockFunc();
+    createMemoFromPath(pathV2, mockFn);
+    expect(mockFn).toBeCalledWith('$.address["street","city","zipCode"]');
+    expect(mockFn).toBeCalledTimes(1);
+  });
+
+  test("createMemoFromPath", () => {
+    const memo: MemoBundle = createMemoFromPath(pathV2, newJSONPath);
+    expect(memo.top.scalar).toBeUndefined();
+    expect(memo.top.arrays).toEqual([]);
+    expect(memo.structs).toHaveLength(1);
+    expect(memo.structArrays).toHaveLength(0);
   });
 
   test("memo3", () => {
@@ -146,276 +193,13 @@ describe("address", () => {
         value: "12345",
       },
     ];
-    const memo: PluginValuesPathMemo4 = createMemoFromPath({
-      scalars: path,
-      structArrays: { items: [] },
-      structs: { items: [] },
-    });
-    const result: PluginValues[] = memo3(
+    const memo: MemoBundle = createMemoFromPath(pathV2, newJSONPath);
+    const values: PluginValues[] = runMemoBundle(
       "struct",
       "Address",
       paramObject,
       memo
     );
-    expect(result).toEqual(expectedValues);
-  });
-});
-
-describe("Person", () => {
-  const path: StructPropertysPath = {
-    category: "struct",
-    name: "Person",
-    scalars: `$.person["name","age"]`,
-    scalarArrays: [
-      { path: "$.person.items[*]", param: personScheame.scalarArrays[0] },
-      { path: "$.person.nicknames[*]", param: personScheame.scalarArrays[1] },
-    ],
-    objectSchema: toObjectPluginParams(personScheame.scalars),
-  };
-  const paramObject = {
-    person: {
-      name: "John Doe",
-      age: 30,
-      items: [1, 2, 3],
-      nicknames: ["Johnny", "JD"],
-    } as const satisfies Person,
-  };
-  test("getPathFromStruct", () => {
-    const param = {
-      name: "person",
-      attr: { kind: "struct", struct: "Person" },
-    } as const satisfies PluginParamEx<StructRefParam>;
-    const structMap: ReadonlyMap<string, ClassifiedPluginParams> = new Map([
-      ["Person", personScheame],
-    ]);
-    const result: StructPathResult = getPathFromStructParam(
-      [param],
-      "$",
-      structMap
-    );
-    expect(result.items).toEqual([path]);
-    expect(result.errors).toEqual([]);
-  });
-  test("extractScalaValuesFromJson", () => {
-    const expectedValues: PluginValues[] = [
-      {
-        category: "struct",
-        name: "Person",
-        param: { name: "name", attr: { kind: "string", default: "" } },
-        value: "John Doe",
-      },
-      {
-        category: "struct",
-        name: "Person",
-        param: { name: "age", attr: { kind: "number", default: 0 } },
-        value: 30,
-      },
-      {
-        category: "struct",
-        name: "items",
-        param: {
-          name: "items",
-          attr: { kind: "number[]", default: [] },
-        },
-        value: 1,
-      },
-      {
-        category: "struct",
-        name: "items",
-        param: {
-          name: "items",
-          attr: { kind: "number[]", default: [] },
-        },
-        value: 2,
-      },
-      {
-        category: "struct",
-        name: "items",
-        value: 3,
-        param: {
-          attr: {
-            default: [],
-            kind: "number[]",
-          },
-          name: "items",
-        },
-      },
-      {
-        category: "struct",
-        name: "nicknames",
-        value: "Johnny",
-        param: {
-          name: "nicknames",
-          attr: { kind: "string[]", default: [] },
-        },
-      },
-      {
-        category: "struct",
-        name: "nicknames",
-        value: "JD",
-        param: {
-          name: "nicknames",
-          attr: {
-            kind: "string[]",
-            default: [],
-          },
-        },
-      },
-    ];
-    const memo = createMemoFromPath({
-      scalars: path,
-      structArrays: { items: [] },
-      structs: { items: [] },
-    });
-    const result: PluginValues[] = memo3("struct", "Person", paramObject, memo);
-    expect(result).toEqual(expectedValues);
-  });
-});
-
-describe("classRoom", () => {
-  const paths: StructPropertysPath[] = [
-    {
-      category: "struct",
-      name: "Class",
-      scalars: `$.classRoom["className"]`,
-      scalarArrays: [],
-      objectSchema: toObjectPluginParams(classRoomSchema.scalars),
-    },
-    {
-      category: "struct",
-      name: "Person",
-      objectSchema: {
-        age: {
-          default: 0,
-          kind: "number",
-        },
-        name: {
-          default: "",
-          kind: "string",
-        },
-      },
-      scalarArrays: [
-        {
-          param: {
-            attr: {
-              default: [],
-              kind: "number[]",
-            },
-            name: "items",
-          },
-          path: "$.classRoom.teacher.items[*]",
-        },
-        {
-          param: {
-            attr: {
-              default: [],
-              kind: "string[]",
-            },
-            name: "nicknames",
-          },
-          path: "$.classRoom.teacher.nicknames[*]",
-        },
-      ],
-      scalars: '$.classRoom.teacher["name","age"]',
-    },
-    {
-      category: "struct",
-      name: "Person",
-      objectSchema: {
-        age: {
-          default: 0,
-          kind: "number",
-        },
-        name: {
-          default: "",
-          kind: "string",
-        },
-      },
-      scalarArrays: [
-        {
-          param: {
-            attr: {
-              default: [],
-              kind: "number[]",
-            },
-            name: "items",
-          },
-          path: "$.classRoom.students[*].items[*]",
-        },
-        {
-          param: {
-            attr: {
-              default: [],
-              kind: "string[]",
-            },
-            name: "nicknames",
-          },
-          path: "$.classRoom.students[*].nicknames[*]",
-        },
-      ],
-      scalars: '$.classRoom.students[*]["name","age"]',
-    },
-  ];
-  const paramObject = {
-    classRoom: {
-      className: "Class A",
-      teacher: {
-        name: "Ms. Smith",
-        age: 40,
-        items: [],
-        nicknames: [],
-      } satisfies Person,
-      students: [
-        {
-          name: "Alice",
-          age: 12,
-          items: [10, 20],
-          nicknames: ["Ally"],
-        },
-        {
-          name: "Bob",
-          age: 13,
-          items: [30],
-          nicknames: ["Bobby", "Rob"],
-        },
-      ],
-    } as const satisfies Class,
-  };
-  test("getPathFromStruct", () => {
-    const param = {
-      name: "classRoom",
-      attr: { kind: "struct", struct: "Class" },
-    } as const satisfies PluginParamEx<StructRefParam>;
-    const structMap: ReadonlyMap<string, ClassifiedPluginParams> = makeMap();
-    const result: StructPathResult = getPathFromStructParam(
-      [param],
-      "$",
-      structMap
-    );
-    expect(result.items).toEqual(paths);
-    expect(result.errors).toEqual([]);
-  });
-  test("memo3", () => {
-    const expectedValues: PluginValues[] = [
-      {
-        category: "struct",
-        name: "Class",
-        value: "Class A",
-        param: {
-          name: "className",
-          attr: {
-            default: "",
-            kind: "string",
-          },
-        },
-      },
-    ];
-    const memo = createMemoFromPath({
-      scalars: paths[0],
-      structArrays: { items: [paths[2]] },
-      structs: { items: [paths[1]] },
-    });
-    const result: PluginValues[] = memo3("struct", "Class", paramObject, memo);
-    expect(result).toEqual(expectedValues);
+    expect(values).toEqual(expectedValues);
   });
 });
