2996d083c8e55bc3320089c43667f18e6231c9c1
Sigureya <sigureya1208@gmail.com>
2025-11-15 14:36:30 +0900

    必須にしても動くようになった

--- src/features/JSONPath/core/createPath/types/pathSchemaTypes.ts
+++ src/features/JSONPath/core/createPath/types/pathSchemaTypes.ts
@@ -37,7 +37,7 @@ export interface PluginValuesPathWithError {
 
 export interface PluginValuesPathNewVersion {
   category: ValueCategory;
-  name?: string;
+  name: string;
   scalars: StructPropertysPath;
   structs: StructPathResult;
   structArrays: StructPathResult;
