f8c9959edb3ce5c56bba256439939396697dfa6a
Sigureya <sigureya1208@gmail.com>
2025-11-25 01:41:49 +0900

    新しい関数を追加して再ビルド

--- dist/types/rmmz/plugin/core/rmmzJSON/parseDeepJSON.d.ts
+++ dist/types/rmmz/plugin/core/rmmzJSON/parseDeepJSON.d.ts
@@ -1,2 +1,3 @@
 import { JSONValue } from '../../../../libs/jsonPath';
 export declare const parseDeepJSON: (json: string) => JSONValue;
+export declare const parseDeepRecord: (record: Record<string, string>) => JSONValue;
