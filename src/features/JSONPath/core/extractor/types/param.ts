95e9fd7d8326086d105fcad24c32e979fd6c63a0
Sigureya <sigureya1208@gmail.com>
2025-11-30 21:14:37 +0900

    プラグインパラメータ抽出用

--- src/features/JSONPath/core/extractor/types/param.ts
+++ src/features/JSONPath/core/extractor/types/param.ts
@@ -1,1 +1,12 @@
-export interface PluginParamExtractor {}
+import type { ExtractorBundle } from "./bundle";
+import type { PluginValues } from "./result";
+
+export interface PluginParamExtractor {
+  pluginName: string;
+  extractors: ExtractorBundle[];
+}
+
+export interface ParamExtractResult {
+  pluginName: string;
+  params: PluginValues[];
+}
