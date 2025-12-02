3654988426c95d68e4b707e8d77c9f5add9fe57f
Sigureya <sigureya1208@gmail.com>
2025-12-02 14:44:58 +0900

    PluginParamのstructを正式対応させた

--- dist/types/features/JSONPath/core/extractor/types/param.d.ts
+++ dist/types/features/JSONPath/core/extractor/types/param.d.ts
@@ -1,5 +1,12 @@
+import { PluginParam } from '../../../../../rmmz/plugin';
 import { ExtractorBundle } from './bundle';
 import { PluginValues } from './result';
+export interface PluginParamsSchema {
+    pluginName: string;
+    schema: {
+        params: PluginParam[];
+    };
+}
 export interface PluginParamExtractor {
     pluginName: string;
     extractors: ExtractorBundle[];
