6e67e82e3f73509c4caca2f88dea0d2b55178885
Sigureya <sigureya1208@gmail.com>
2025-12-03 19:37:50 +0900

    テンプレート対応の準備

--- src/features/JSONPath/core/extractor/types/bundle.ts
+++ src/features/JSONPath/core/extractor/types/bundle.ts
@@ -3,22 +3,22 @@ import type { ScalarParam } from "@RmmzPluginSchema/rmmz/plugin";
 import type { ArrayPathExtractor } from "./array";
 import type { ValueCategory2 } from "./result";
 
-export interface PluginValuesPathMemo4 {
-  scalar?: ScalarValueExtractor;
+export interface PluginValuesPathMemo4<S extends ScalarParam = ScalarParam> {
+  scalar?: ScalarValueExtractor<S>;
   arrays: ArrayPathExtractor[];
   bundleName: string;
 }
 
-export interface ScalarValueExtractor {
+export interface ScalarValueExtractor<S extends ScalarParam = ScalarParam> {
   jsonPathJS: JSONPathReader;
-  record: Record<string, ScalarParam>;
+  record: Record<string, S>;
 }
 
-export interface ExtractorBundle {
+export interface ExtractorBundle<S extends ScalarParam = ScalarParam> {
   rootName: string;
   rootCategory: ValueCategory2;
 
-  top: PluginValuesPathMemo4 | undefined;
-  structs: PluginValuesPathMemo4[];
-  structArrays: PluginValuesPathMemo4[];
+  top: PluginValuesPathMemo4<S> | undefined;
+  structs: PluginValuesPathMemo4<S>[];
+  structArrays: PluginValuesPathMemo4<S>[];
 }
