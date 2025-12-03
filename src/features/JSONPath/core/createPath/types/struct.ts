54c967ddb3d2e47d2d1a7762c1223cb1488b28f1
Sigureya <sigureya1208@gmail.com>
2025-12-03 19:57:36 +0900

    一時的にテンプレートによるtypeに切り替え

--- src/features/JSONPath/core/createPath/types/struct.ts
+++ src/features/JSONPath/core/createPath/types/struct.ts
@@ -2,18 +2,22 @@ import type {
   ArrayParamTypes,
   ScalarParam,
 } from "@RmmzPluginSchema/rmmz/plugin";
-import type { StructPathError } from "./errorTypes";
-import type { TemplateE, TemplateG, StructPropertysPathEx3 } from "./template";
+import type {
+  TemplateE,
+  TemplateG,
+  StructPropertysPathEx3,
+  TemplateGE,
+} from "./template";
 
 export type StructPropertysPath = StructPropertysPathEx3<
   ScalarParam,
   ArrayParamTypes
 >;
 
-export interface StructPathResultWithError {
-  items: StructPropertysPath[];
-  errors: StructPathError[];
-}
+export type StructPathResultWithError = TemplateGE<
+  ScalarParam,
+  ArrayParamTypes
+>;
 
 export type PluginValuesPathWithError = TemplateE<ScalarParam, ArrayParamTypes>;
 
