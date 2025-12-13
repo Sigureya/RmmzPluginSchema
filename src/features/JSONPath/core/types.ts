41da29d6724ae718a51f4d2836a436ec0a083630
Sigureya <sigureya1208@gmail.com>
2025-12-13 23:24:08 +0900

    戻り値用の型。名前は後で変える

--- src/features/JSONPath/core/types.ts
+++ src/features/JSONPath/core/types.ts
@@ -1,5 +1,12 @@
 import type {
+  PluginScalarParam,
+  PluginArrayParamType,
+  PluginParam,
+  ClassifiedPluginParamsEx2,
+} from "@RmmzPluginSchema/rmmz/plugin";
+import type {
   CommandExtractorEntry,
+  PluginValues,
   PluginValuesExtractorBundle,
 } from "./extractor/types";
 
@@ -7,4 +14,13 @@ export interface PluginExtractorBundle {
   pluginName: string;
   params: PluginValuesExtractorBundle[];
   commands: CommandExtractorEntry[];
+}
+
+export interface X<
+  S extends PluginScalarParam,
+  A extends PluginArrayParamType
+> {
+  pluginName: string;
+  params: PluginValues<PluginParam>[];
+  structMap: Map<string, ClassifiedPluginParamsEx2<S, A>>;
 }
