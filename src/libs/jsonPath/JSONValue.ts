4bb04b3b57715dfa6d141c4fd57bb349bc39d6d3
Sigureya <sigureya1208@gmail.com>
2025-12-14 20:03:20 +0900

    エディタ上での表示の都合で修正

--- src/libs/jsonPath/JSONValue.ts
+++ src/libs/jsonPath/JSONValue.ts
@@ -1,5 +1,4 @@
 type JsonPrimitive = string | number | boolean | null;
-type JsonObject = { [key: string]: JsonValueType };
-type JsonArray = JsonValueType[];
-type JsonValueType = JsonPrimitive | JsonArray | JsonObject;
-export type JSONValue = JsonValueType;
+type JsonObject = { [key: string]: JSONValue };
+type JsonArray = JSONValue[];
+export type JSONValue = JsonPrimitive | JsonArray | JsonObject;
