19a40ba38f3f12cdf8776fc304df4a786320e011
Sigureya <sigureya1208@gmail.com>
2025-12-23 15:55:21 +0900

    複数のlocaleに対応

--- src/rmmz/plugin/core/parse/block.test.ts
+++ src/rmmz/plugin/core/parse/block.test.ts
@@ -1,5 +1,5 @@
-import { describe, test, expect, it } from "vitest";
-import type { Block, PlguinBodyBlock } from "./block";
+import { describe, expect, it } from "vitest";
+import type { Block, PlguinBodyBlock, PlguinStructBlock } from "./block";
 import { splitBlock } from "./block";
 
 describe("splitBlock", () => {
@@ -43,16 +43,16 @@ describe("splitBlock", () => {
 
     const block: string = lines.join("\n");
     const result: Block = splitBlock(block);
-    test("should split block into bodies", () => {
+    it("should split block into bodies", () => {
       expect(result.bodies).toEqual(expected.bodies);
     });
-    test("should split block into structs", () => {
+    it("should split block into structs", () => {
       expect(result.structs).toEqual(expected.structs);
     });
   });
 
   describe("locale in body", () => {
-    test("should handle locale in body", () => {
+    it("should handle locale in body", () => {
       const lines: string[] = ["/*:ja", "@plugindesc モック", "*/"];
       const expectedBodies: PlguinBodyBlock[] = [
         {
@@ -64,9 +64,7 @@ describe("splitBlock", () => {
       const result = splitBlock(lines.join("\n"));
       expect(result.bodies).toEqual(expectedBodies);
     });
-  });
 
-  describe("multiple locales", () => {
     it("should handle multiple locales", () => {
       const lines: string[] = [
         "/*:",
@@ -88,6 +86,159 @@ describe("splitBlock", () => {
 
       const result = splitBlock(lines.join("\n"));
       expect(result.bodies).toEqual(expectedBodies);
+    });
+    it("should handle locale in both body and struct", () => {
+      const lines: string[] = [
+        "/*:ja",
+        "@plugindesc モック",
+        "*/",
+        "/*~struct~Vector2:ja",
+        "@param x",
+        "@type number",
+        "@default 0",
+        "@param y",
+        "@type number",
+        "@default 0",
+        "*/",
+      ];
+      const expectedBodies: PlguinBodyBlock[] = [
+        {
+          locale: "ja",
+          lines: ["@plugindesc モック"],
+        },
+      ];
+      const expectedStructs: PlguinStructBlock[] = [
+        {
+          struct: "Vector2",
+          locale: "ja",
+          lines: [
+            "@param x",
+            "@type number",
+            "@default 0",
+            "@param y",
+            "@type number",
+            "@default 0",
+          ],
+        },
+      ];
+      const result = splitBlock(lines.join("\n"));
+      expect(result.bodies).toEqual(expectedBodies);
+      expect(result.structs).toEqual(expectedStructs);
+    });
+    it("should handle struct without locale", () => {
+      const lines: string[] = [
+        "/*:ja",
+        "@plugindesc モック",
+        "*/",
+        "/*~struct~Vector2",
+        "@param x",
+        "@type number",
+        "@default 0",
+        "@param y",
+        "@type number",
+        "@default 0",
+        "*/",
+      ];
+      const expectedBodies: PlguinBodyBlock[] = [
+        {
+          locale: "ja",
+          lines: ["@plugindesc モック"],
+        },
+      ];
+      const expectedStructs: PlguinStructBlock[] = [
+        {
+          struct: "Vector2",
+          lines: [
+            "@param x",
+            "@type number",
+            "@default 0",
+            "@param y",
+            "@type number",
+            "@default 0",
+          ],
+        },
+      ];
+      const result = splitBlock(lines.join("\n"));
+      expect(result.bodies).toEqual(expectedBodies);
+      expect(result.structs).toEqual(expectedStructs);
+    });
+    it("full locale test", () => {
+      const lines: string[] = [
+        "/*:",
+        "@plugindesc mock",
+        "*/",
+
+        "/*:ja",
+        "@plugindesc モック",
+        "*/",
+
+        "/*:ge",
+        "@plugindesc Spott",
+        "*/",
+
+        "/*~struct~Vector2:ja",
+        "@param x",
+        "@type number",
+        "@text X座標",
+        "@default 0",
+        "@param y",
+        "@type number",
+        "@text Y座標",
+        "@default 0",
+        "*/",
+
+        "/*~struct~Vector2",
+        "@param x",
+        "@type number",
+        "@default 0",
+        "@param y",
+        "@type number",
+        "@default 0",
+        "*/",
+      ];
+      const expectedBodies: PlguinBodyBlock[] = [
+        {
+          lines: ["@plugindesc mock"],
+        },
+        {
+          locale: "ja",
+          lines: ["@plugindesc モック"],
+        },
+        {
+          locale: "ge",
+          lines: ["@plugindesc Spott"],
+        },
+      ];
+      const expectedStructs: PlguinStructBlock[] = [
+        {
+          struct: "Vector2",
+          locale: "ja",
+          lines: [
+            "@param x",
+            "@type number",
+            "@text X座標",
+            "@default 0",
+            "@param y",
+            "@type number",
+            "@text Y座標",
+            "@default 0",
+          ],
+        },
+        {
+          struct: "Vector2",
+          lines: [
+            "@param x",
+            "@type number",
+            "@default 0",
+            "@param y",
+            "@type number",
+            "@default 0",
+          ],
+        },
+      ];
+      const result = splitBlock(lines.join("\n"));
+      expect(result.bodies).toEqual(expectedBodies);
+      expect(result.structs).toEqual(expectedStructs);
     });
   });
   describe("noname struct", () => {
