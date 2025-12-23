1458e9a0103e239c559ec4639efb0931094b6c1c
Sigureya <sigureya1208@gmail.com>
2025-12-24 05:00:26 +0900

    無関係の属性が混ざっていたら消すテスト。

---
+++ src/rmmz/plugin/core/attributes.all.test.ts
@@ -0,0 +1,82 @@
+import { describe, expect, test } from "vitest";
+import { compileAttributes } from "./attributes";
+import type {
+  BooleanParam,
+  NumberArrayParam,
+  NumberParam,
+  PrimitiveParam,
+} from "./params";
+import type { PluginParamTokens, PluginTokensRecord } from "./parse";
+
+describe("compileAttributes", () => {
+  const allTokens: PluginTokensRecord = {
+    min: "0",
+    max: "200",
+    decimals: "2",
+    on: "enable",
+    off: "disable",
+    dir: "img/pictures/",
+    parent: "ppp",
+  };
+  test("number", () => {
+    const tokens: PluginParamTokens = {
+      name: "num",
+      attr: {
+        ...allTokens,
+        kind: "number",
+        default: "123",
+      },
+    };
+
+    const expected: NumberParam = {
+      kind: "number",
+      min: 0,
+      max: 200,
+      decimals: 2,
+      default: 123,
+      parent: "ppp",
+    };
+
+    const result: PrimitiveParam = compileAttributes(tokens);
+    expect(result).toEqual(expected);
+  });
+  test("number[]", () => {
+    const tokens: PluginParamTokens = {
+      name: "numArray",
+      attr: {
+        ...allTokens,
+        kind: "number[]",
+        default: "[10, 20, 30]",
+      },
+    };
+    const expected: NumberArrayParam = {
+      kind: "number[]",
+      min: 0,
+      max: 200,
+      decimals: 2,
+      default: [10, 20, 30],
+      parent: "ppp",
+    };
+    const result: PrimitiveParam = compileAttributes(tokens);
+    expect(result).toEqual(expected);
+  });
+  test("boolean", () => {
+    const tokens: PluginParamTokens = {
+      name: "bool",
+      attr: {
+        ...allTokens,
+        kind: "boolean",
+        default: "true",
+      },
+    };
+    const expected: BooleanParam = {
+      kind: "boolean",
+      on: "enable",
+      off: "disable",
+      default: true,
+      parent: "ppp",
+    };
+    const result: PrimitiveParam = compileAttributes(tokens);
+    expect(result).toEqual(expected);
+  });
+});
