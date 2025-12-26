210d0d0a8f221d6aa2428a042e1d5933a3338a41
Sigureya <sigureya1208@gmail.com>
2025-12-27 01:17:53 +0900

    パース関数を切り換えられるようにした。またどこから来たかも判定しやすくした

--- src/rmmz/plugin/core/compilePluginAsArraySchema.ts
+++ src/rmmz/plugin/core/compilePluginAsArraySchema.ts
@@ -11,44 +11,58 @@ import type {
   PluginCommandTokens,
   StructParseState,
 } from "./parse/types";
+import { parseDeepJSON } from "./rmmzJSON";
+
+export type CCC = "command" | "param" | "struct";
 
 export const compilePluginAsArraySchema = (
-  parsedPlugin: PluginTokens
+  parsedPlugin: PluginTokens,
+  fn: (structDefault: string, category: CCC) => unknown = (s: string) =>
+    parseDeepJSON(s)
 ): PluginSchemaArray => ({
-  commands: mapCommands(parsedPlugin.commands),
-  params: mapParams(parsedPlugin.params),
-  structs: mapStructs(parsedPlugin.structs),
+  params: mapParams(parsedPlugin.params, (text: string) => fn(text, "param")),
+  commands: mapCommands(parsedPlugin.commands, (text: string) =>
+    fn(text, "command")
+  ),
+  structs: mapStructs(parsedPlugin.structs, (text: string) =>
+    fn(text, "struct")
+  ),
 });
 
-const mapParams = (params: ReadonlyArray<PluginParamTokens>): PluginParam[] => {
+const mapParams = (
+  params: ReadonlyArray<PluginParamTokens>,
+  fn: (structDefault: string) => unknown
+): PluginParam[] => {
   return params.map(
     (p): PluginParam => ({
       name: p.name,
-      attr: compileAttributes(p),
+      attr: compileAttributes(p, fn),
     })
   );
 };
 
 const mapCommands = (
-  commands: ReadonlyArray<PluginCommandTokens>
+  commands: ReadonlyArray<PluginCommandTokens>,
+  fn: (structDefault: string) => unknown
 ): PluginCommandSchemaArray[] => {
   return commands.map(
     (cmd): PluginCommandSchemaArray => ({
       command: cmd.command,
       desc: cmd.desc,
       text: cmd.text,
-      args: mapParams(cmd.args),
+      args: mapParams(cmd.args, fn),
     })
   );
 };
 
 const mapStructs = (
-  structs: ReadonlyArray<StructParseState>
+  structs: ReadonlyArray<StructParseState>,
+  fn: (structDefault: string) => unknown
 ): PluginStructSchemaArray[] => {
   return structs.map(
     (s): PluginStructSchemaArray => ({
       struct: s.name,
-      params: mapParams(s.params),
+      params: mapParams(s.params, fn),
     })
   );
 };
