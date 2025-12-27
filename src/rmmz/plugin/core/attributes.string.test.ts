ddee62a86cb1b3666726d2a2ea5c28c4def7d217
Sigureya <sigureya1208@gmail.com>
2025-12-28 01:46:39 +0900

    describeの名前部分を修正。このように段落分けした方が良さそう

--- src/rmmz/plugin/core/attributes.string.test.ts
+++ src/rmmz/plugin/core/attributes.string.test.ts
@@ -3,86 +3,87 @@ import type { ParamSoruceRecord } from "./attributes";
 import { compileAttributes } from "./attributes";
 import type { StringParam, StringArrayParam } from "./params";
 import type { PluginParamTokens } from "./parse";
-
-describe("compileAttributes - string", () => {
-  test("minimum set", () => {
-    const tokens: PluginParamTokens = {
-      name: "attr",
-      attr: {
+describe("compileAttributes", () => {
+  describe("string", () => {
+    test("minimum set", () => {
+      const tokens: PluginParamTokens = {
+        name: "attr",
+        attr: {
+          kind: "string",
+          default: "abc",
+        } satisfies ParamSoruceRecord<StringParam>,
+      };
+      const expected: StringParam = {
         kind: "string",
         default: "abc",
-      } satisfies ParamSoruceRecord<StringParam>,
-    };
-    const expected: StringParam = {
-      kind: "string",
-      default: "abc",
-    };
-    const fn = vi.fn(() => {});
-    const result = compileAttributes(tokens, fn);
-    expect(result).toEqual(expected);
-    expect(fn).not.toHaveBeenCalled();
-  });
+      };
+      const fn = vi.fn(() => {});
+      const result = compileAttributes(tokens, fn);
+      expect(result).toEqual(expected);
+      expect(fn).not.toHaveBeenCalled();
+    });
 
-  test("full set", () => {
-    const tokens: PluginParamTokens = {
-      name: "attr",
-      attr: {
+    test("full set", () => {
+      const tokens: PluginParamTokens = {
+        name: "attr",
+        attr: {
+          kind: "string",
+          default: "abc",
+          text: "String Text",
+          desc: "String Description",
+          parent: "Parent String",
+        } satisfies ParamSoruceRecord<StringParam>,
+      };
+      const expected: StringParam = {
         kind: "string",
         default: "abc",
         text: "String Text",
         desc: "String Description",
         parent: "Parent String",
-      } satisfies ParamSoruceRecord<StringParam>,
-    };
-    const expected: StringParam = {
-      kind: "string",
-      default: "abc",
-      text: "String Text",
-      desc: "String Description",
-      parent: "Parent String",
-    };
-    const fn = vi.fn(() => {});
-    const result = compileAttributes(tokens, fn);
-    expect(result).toEqual(expected);
-    expect(fn).not.toHaveBeenCalled();
-  });
-});
-
-describe("compileAttributes - string[]", () => {
-  test("minimum set", () => {
-    const tokens: PluginParamTokens = {
-      name: "attr",
-      attr: {
-        kind: "string[]",
-        default: `["a", "b", "c"]`,
-      } satisfies ParamSoruceRecord<StringArrayParam>,
-    };
-    const expected: StringArrayParam = {
-      kind: "string[]",
-      default: ["a", "b", "c"],
-    };
-    const fn = vi.fn(() => {});
-
-    const result = compileAttributes(tokens, fn);
-    expect(result).toEqual(expected);
-    expect(fn).not.toHaveBeenCalled();
+      };
+      const fn = vi.fn(() => {});
+      const result = compileAttributes(tokens, fn);
+      expect(result).toEqual(expected);
+      expect(fn).not.toHaveBeenCalled();
+    });
   });
 
-  test("empty array", () => {
-    const tokens: PluginParamTokens = {
-      name: "attr",
-      attr: {
+  describe("string[]", () => {
+    test("minimum set", () => {
+      const tokens: PluginParamTokens = {
+        name: "attr",
+        attr: {
+          kind: "string[]",
+          default: `["a", "b", "c"]`,
+        } satisfies ParamSoruceRecord<StringArrayParam>,
+      };
+      const expected: StringArrayParam = {
         kind: "string[]",
-        default: `[]`,
-      } satisfies ParamSoruceRecord<StringArrayParam>,
-    };
-    const expected: StringArrayParam = {
-      kind: "string[]",
-      default: [],
-    };
-    const fn = vi.fn(() => {});
-    const result = compileAttributes(tokens, fn);
-    expect(result).toEqual(expected);
-    expect(fn).not.toHaveBeenCalled();
+        default: ["a", "b", "c"],
+      };
+      const fn = vi.fn(() => {});
+
+      const result = compileAttributes(tokens, fn);
+      expect(result).toEqual(expected);
+      expect(fn).not.toHaveBeenCalled();
+    });
+
+    test("empty array", () => {
+      const tokens: PluginParamTokens = {
+        name: "attr",
+        attr: {
+          kind: "string[]",
+          default: `[]`,
+        } satisfies ParamSoruceRecord<StringArrayParam>,
+      };
+      const expected: StringArrayParam = {
+        kind: "string[]",
+        default: [],
+      };
+      const fn = vi.fn(() => {});
+      const result = compileAttributes(tokens, fn);
+      expect(result).toEqual(expected);
+      expect(fn).not.toHaveBeenCalled();
+    });
   });
 });
