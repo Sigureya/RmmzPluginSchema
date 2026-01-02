629f33f570ed0f252770400f013a41a68e15b01a
Sigureya <sigureya1208@gmail.com>
2026-01-02 10:49:44 +0900

    キーワード１行部分を生成する関数。出力方式を変更しても動くようにするためにこの方式にした

---
+++ src/features/genarator/keywordLine.ts
@@ -0,0 +1,23 @@
+import { KEYWORD_TYPE } from "@RmmzPluginSchema/rmmz/plugin/core/parse";
+import type { KeyWord } from "./types/keyword";
+
+export const createKeywordLine = <K extends string>(
+  keyword: K,
+  value: string
+): KeyWord<K> => {
+  return `@${keyword} ${value}`;
+};
+
+export const createKeywordLineEx = <T>(
+  data: T,
+  key: Extract<keyof T, string>
+): KeyWord<Extract<typeof key, string>> | undefined => {
+  const value = data[key];
+  return value === undefined
+    ? undefined
+    : createKeywordLine(key, String(value));
+};
+
+export const createKindLine = (data: { kind: string }) => {
+  return createKeywordLine(KEYWORD_TYPE, data.kind);
+};
