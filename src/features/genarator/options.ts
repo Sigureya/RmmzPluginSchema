e88ea12faaf53ff516682ba55c0e078172910c1f
Sigureya <sigureya1208@gmail.com>
2026-01-02 12:08:25 +0900

    オプションの変換。単純な関数なのでコメントなし

---
+++ src/features/genarator/options.ts
@@ -0,0 +1,22 @@
+import type { ComboParam, SelectParam } from "@RmmzPluginSchema/rmmz/plugin";
+import type { OptionItem } from "@RmmzPluginSchema/rmmz/plugin/core/parse";
+import { KEYWORD_OPTION } from "@RmmzPluginSchema/rmmz/plugin/core/parse";
+import { createKeywordLine } from "./keywordLine";
+import type { KeyWord } from "./types/keyword";
+
+export const generateSelectOptions = (
+  select: SelectParam
+): (KeyWord<"option"> | KeyWord<"value">)[] => {
+  return select.options.flatMap((opt: OptionItem) => [
+    createKeywordLine(KEYWORD_OPTION, opt.option),
+    createKeywordLine("value", opt.value),
+  ]);
+};
+
+export const generateComboOptions = (
+  combo: ComboParam
+): KeyWord<"option">[] => {
+  return combo.options.map((opt: string) =>
+    createKeywordLine(KEYWORD_OPTION, opt)
+  );
+};
