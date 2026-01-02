515338cdb7cc9172bb6dcdf348d2991d76fe4974
Sigureya <sigureya1208@gmail.com>
2026-01-02 10:45:43 +0900

    出力用の定義を追加

--- src/features/genarator/types/types.ts
+++ src/features/genarator/types/types.ts
@@ -1,7 +1,10 @@
 import type {
+  KEYWORD_AUTHOR,
   KEYWORD_BASE,
+  KEYWORD_DESC,
   KEYWORD_ORDERAFTER,
   KEYWORD_ORDERBEFORE,
+  KEYWORD_URL,
 } from "@RmmzPluginSchema/rmmz/plugin/core/parse";
 import type { KeyWord } from "./keyword";
 
@@ -10,3 +13,8 @@ export interface Annotation_PluginDependencies {
   orderBefore: KeyWord<typeof KEYWORD_ORDERBEFORE>[];
   orderAfter: KeyWord<typeof KEYWORD_ORDERAFTER>[];
 }
+
+export type Annotation_Meta =
+  | KeyWord<typeof KEYWORD_AUTHOR>
+  | KeyWord<typeof KEYWORD_DESC>
+  | KeyWord<typeof KEYWORD_URL>;
