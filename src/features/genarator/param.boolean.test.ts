09801b75028b24596f94a131954169d2a48e4624
Sigureya <sigureya1208@gmail.com>
2026-01-03 12:31:17 +0900

    boolean型のテスト

---
+++ src/features/genarator/param.boolean.test.ts
@@ -0,0 +1,80 @@
+import type { MockedObject } from "vitest";
+import { describe, expect, test, vi } from "vitest";
+import type {
+  BooleanParam,
+  PluginParamEx,
+} from "@RmmzPluginSchema/rmmz/plugin";
+import { genaratePluginParam } from "./param";
+import type { PluginParamAnnotation } from "./types/schema";
+import type { StringifyXX } from "./types/stringlfy";
+
+const makeHandlers = (): MockedObject<StringifyXX> => {
+  return {
+    numberArray: vi.fn(),
+    structArray: vi.fn(),
+    stringArray: vi.fn(),
+    struct: vi.fn(),
+  };
+};
+
+describe("boolean param", () => {
+  test("true default", () => {
+    const param: PluginParamEx<BooleanParam> = {
+      name: "isEnabled",
+      attr: {
+        kind: "boolean",
+        desc: "Enable or disable the feature.",
+        on: "Yes",
+        off: "No",
+        default: true,
+      },
+    };
+
+    const expected: PluginParamAnnotation = {
+      name: "@param isEnabled",
+      default: "@default true",
+      base: {
+        kind: "@type boolean",
+        desc: "@desc Enable or disable the feature.",
+      },
+      attr: ["@on Yes", "@off No"],
+    };
+    const handlers = makeHandlers();
+    const annotation = genaratePluginParam(param, "param", handlers);
+    expect(annotation).toEqual(expected);
+    expect(handlers.numberArray).not.toHaveBeenCalled();
+    expect(handlers.structArray).not.toHaveBeenCalled();
+    expect(handlers.stringArray).not.toHaveBeenCalled();
+    expect(handlers.struct).not.toHaveBeenCalled();
+  });
+  test("false default", () => {
+    const param: PluginParamEx<BooleanParam> = {
+      name: "key memory",
+      attr: {
+        kind: "boolean",
+        desc: "when key is released, restore previous state.",
+        text: "Enable memory",
+        on: "enable",
+        off: "disable",
+        default: false,
+      },
+    };
+    const expected: PluginParamAnnotation = {
+      name: "@param isEnabled",
+      default: "@default false",
+      base: {
+        kind: "@type boolean",
+        desc: "@desc when key is released, restore previous state.",
+        text: "@text Enable memory",
+      },
+      attr: ["@on enable", "@off disable"],
+    };
+    const handlers = makeHandlers();
+    const annotation = genaratePluginParam(param, "param", handlers);
+    expect(annotation).toEqual(expected);
+    expect(handlers.numberArray).not.toHaveBeenCalled();
+    expect(handlers.structArray).not.toHaveBeenCalled();
+    expect(handlers.stringArray).not.toHaveBeenCalled();
+    expect(handlers.struct).not.toHaveBeenCalled();
+  });
+});
