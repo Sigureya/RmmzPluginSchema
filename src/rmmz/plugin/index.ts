a37b0972914b62adea715a4912a7abb515e1938c
Sigureya <sigureya1208@gmail.com>
2026-01-04 21:38:51 +0900

    localeは存在しない場合がある。それにも対応

--- src/rmmz/plugin/index.ts
+++ src/rmmz/plugin/index.ts
@@ -4,6 +4,7 @@ export type {
   ClassifiedPluginParams,
   ClassifiedPluginParamsEx,
 } from "./core/params/types/classifyTypes";
+export type { DeepJSONParserHandlers } from "./core/deepJSONHandler";
 export {
   parsePluginParamObject,
   pluginSourceToArraySchema,
