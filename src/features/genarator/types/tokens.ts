8e07def9970a25a5a4059156825c522576c0873f
Sigureya <sigureya1208@gmail.com>
2026-01-04 12:18:58 +0900

    ほぼ文字列に近い状態の配列。この型はjoinして出力に使う。またlocaleがこのタイミングで必要だと判明した

--- src/features/genarator/types/tokens.ts
+++ src/features/genarator/types/tokens.ts
@@ -3,8 +3,21 @@ import type { PluginSchemaAnnotation } from "./schema";
 import type { Annotation_Meta, Annotation_PluginDependencies } from "./types";
 
 export interface AnnotationTokens {
+  locale?: string | undefined;
   schema: PluginSchemaAnnotation;
   meta: Annotation_Meta;
   target: KeyWord<"target">;
   dependencies: Annotation_PluginDependencies;
 }
+
+export type StructTokenBlock = [
+  head: `/*~struct~${string}:${string}`,
+  ...("" | KeyWord<string>)[],
+  tail: "*/"
+];
+
+export type PluginBodyBlockToken = [
+  head: `/*:${string}`,
+  ...string[],
+  tail: `*/`
+];
