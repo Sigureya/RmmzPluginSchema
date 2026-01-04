9421a34ae649de6a2ab7c154a9a9966f865a37d9
Sigureya <sigureya1208@gmail.com>
2026-01-04 18:36:56 +0900

    オプション変換のテスト。まあこれでOKでしょう

--- src/features/genarator/options.ts
+++ src/features/genarator/options.ts
@@ -1,21 +1,24 @@
 import type { ComboParam, SelectParam } from "@RmmzPluginSchema/rmmz/plugin";
 import type { OptionItem } from "@RmmzPluginSchema/rmmz/plugin/core/parse";
-import { KEYWORD_OPTION } from "@RmmzPluginSchema/rmmz/plugin/core/parse";
+import {
+  KEYWORD_OPTION,
+  KEYWORD_VALUE,
+} from "@RmmzPluginSchema/rmmz/plugin/core/parse";
 import { createKeywordLine } from "./keywordLine";
 import type { KeyWord } from "./types";
 
 export const generateSelectOptions = (
   select: SelectParam
-): (KeyWord<"option"> | KeyWord<"value">)[] => {
+): (KeyWord<typeof KEYWORD_OPTION> | KeyWord<typeof KEYWORD_VALUE>)[] => {
   return select.options.flatMap((opt: OptionItem) => [
     createKeywordLine(KEYWORD_OPTION, opt.option),
-    createKeywordLine("value", opt.value),
+    createKeywordLine(KEYWORD_VALUE, opt.value),
   ]);
 };
 
 export const generateComboOptions = (
   combo: ComboParam
-): KeyWord<"option">[] => {
+): KeyWord<typeof KEYWORD_OPTION>[] => {
   return combo.options.map((opt: string) =>
     createKeywordLine(KEYWORD_OPTION, opt)
   );
