24b959731714abab0a2c907693bd089bd2f23fe0
Sigureya <sigureya1208@gmail.com>
2026-03-17 19:43:54 +0900

    非推奨関数の切り替え。戻り値が変わった

--- dist/types/rmmz/plugin/core/attributes.d.ts
+++ dist/types/rmmz/plugin/core/attributes.d.ts
@@ -1,9 +1,5 @@
 import { DeepJSONParserHandlers } from './deepJSONHandler';
-import { PrimitiveParam, PluginParam } from './params';
+import { PluginParam } from './params';
 import { PluginParamTokens } from './parse';
 export type ParamSoruceRecord<T> = Partial<Record<keyof T, string>>;
 export declare const compilePluginParam: (tokens: PluginParamTokens, handlers: DeepJSONParserHandlers) => PluginParam;
-/**
- * @deprecated Use `compilePluginParam` instead.
- */
-export declare const compileAttributes: (tokens: PluginParamTokens, handlers: DeepJSONParserHandlers) => PrimitiveParam;
