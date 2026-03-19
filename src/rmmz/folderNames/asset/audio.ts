8f16ccf6e9ef246464099b60536a6793bdc8c1fb
Sigureya <sigureya1208@gmail.com>
2026-03-19 10:56:52 +0900

    恐らく間違って混入している。消してもビルド可能だったので消す

--- src/rmmz/folderNames/asset/audio.ts
+++
@@ -1,4 +0,0 @@
-export const FOLDER_AUDIO_BGM = "bgm" as const;
-export const FOLDER_AUDIO_SE = "se" as const;
-export const FOLDER_AUDIO_ME = "me" as const;
-export const FOLDER_AUDIO_BGS = "bgs" as const;
