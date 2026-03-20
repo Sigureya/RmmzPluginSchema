c463ca6398c374047f2881c8efb755c88751576a
Sigureya <sigureya1208@gmail.com>
2026-03-20 19:48:07 +0900

    破壊的変更込みで再ビルド。プラグインパラメータのパースエラーが深刻なので、例外を細かく制御することにした

--- dist/types/rmmz/plugin/pluginsJS/jsToJSON.d.ts
+++ dist/types/rmmz/plugin/pluginsJS/jsToJSON.d.ts
@@ -1,3 +1,3 @@
-import { PluginParamsRecord } from './types';
+import { MessageOfparsePluginParamRecord, ResultOfparsePluginParamRecord } from './types/result2';
 export declare const convertPluginsJSToJSON: (src: string) => string[];
-export declare const parsePluginParamRecord: (src: string) => PluginParamsRecord[];
+export declare const parsePluginParamRecord: (src: string, msg: MessageOfparsePluginParamRecord) => ResultOfparsePluginParamRecord;
--- src/rmmz/plugin/index.ts
+++ src/rmmz/plugin/index.ts
@@ -5,8 +5,4 @@ export type {
   ClassifiedPluginParamsEx,
 } from "./core/params/types/classifyTypes";
 export type { DeepJSONParserHandlers } from "./core/deepJSONHandler";
-export {
-  parsePluginParamObject,
-  pluginSourceToArraySchema,
-  pluginSourceToJSON,
-} from "./plugin";
+export { pluginSourceToArraySchema, pluginSourceToJSON } from "./plugin";
