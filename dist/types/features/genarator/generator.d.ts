cc01b42cd10ea61ff2d7bcbcae50a13464053bf0
Sigureya <sigureya1208@gmail.com>
2026-03-22 23:40:51 +0900

    ハンドラ戻り値で使う型をexportなど

--- dist/types/features/genarator/generator.d.ts
+++ dist/types/features/genarator/generator.d.ts
@@ -1,4 +1,5 @@
 import { PluginSchema } from '../../rmmz/plugin';
 import { SchemaStringifyHandlers, PluginAnnotationLines, PluginAnnotationTokens } from './types';
+export declare const generatePluginAnnotationText: (lines: PluginAnnotationLines) => string;
 export declare const generatePluginAnnotationLines: (plugin: PluginSchema, handlers: SchemaStringifyHandlers) => PluginAnnotationLines;
 export declare const generatePluginAnnotation: (plugin: PluginSchema, handlers: SchemaStringifyHandlers) => PluginAnnotationTokens;
