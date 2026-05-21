f0db00905f5451c8f94e3d073220664d2e07a46d
Sigureya <sigureya1208@gmail.com>
2026-05-21 22:24:51 +0900

    最新ビルド

--- dist/types/fileio/read.d.ts
+++ dist/types/fileio/read.d.ts
@@ -1,5 +1,4 @@
-import { ResultOfparsePluginParamRecord } from '../rmmz/plugin';
-import { PluginReadResult } from './convert';
-import { MessageOfparsePluginParamRecordEx } from './types/msg';
-export declare const readPluginInfosSafe: (messages: MessageOfparsePluginParamRecordEx, readPluginInfos: () => Promise<string>) => Promise<ResultOfparsePluginParamRecord>;
-export declare const readAllPluginBodies: (recordsResult: ResultOfparsePluginParamRecord, messages: MessageOfparsePluginParamRecordEx, readPluginBody: (pluginName: string) => Promise<string>) => Promise<PluginReadResult>[];
+import { ParsedPlugin, ResultOfparsePluginParamRecord } from '../rmmz/plugin';
+import { MessageOfparsePluginParamRecordEx, PluginReadResult } from './types/msg';
+export declare const readPluginInfosSafe: (messages: MessageOfparsePluginParamRecordEx, readPluginInfos: () => Promise<string>, parsePluginList: (source: string, msg: MessageOfparsePluginParamRecordEx) => ResultOfparsePluginParamRecord) => Promise<ResultOfparsePluginParamRecord>;
+export declare const readAllPluginBodies: (recordsResult: ResultOfparsePluginParamRecord, messages: MessageOfparsePluginParamRecordEx, readPluginBody: (pluginName: string) => Promise<string>, parsePluginBody: (src: string) => ParsedPlugin) => Promise<PluginReadResult>[];
