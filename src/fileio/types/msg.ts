622f09f5e3e09b38ea66f527590745bb65e54bbb
Sigureya <sigureya1208@gmail.com>
2026-05-21 20:13:26 +0900

    ハンドラを分解していく。パース部分もハンドラに投げられるようにしていく

--- src/fileio/types/msg.ts
+++ src/fileio/types/msg.ts
@@ -1,13 +1,16 @@
-import type { JSONPathReader } from "@RmmzPluginSchema/libs/jsonPath";
-import type { MessageOfparsePluginParamRecord } from "@RmmzPluginSchema/rmmz/plugin";
+import type {
+  MessageOfparsePluginParamRecord,
+  ParsedPlugin,
+  PluginParamsRecord,
+} from "@RmmzPluginSchema/rmmz/plugin";
 
 export interface MessageOfparsePluginParamRecordEx extends MessageOfparsePluginParamRecord {
   readErrorPluginsJS: string;
   readErrorPluginBody: string;
 }
 
-export interface PluginReadHandlers {
-  readPluginInfos: () => Promise<string>;
-  readPluginBody: (pluginName: string) => Promise<string>;
-  jsonPath: (path: string) => JSONPathReader;
+export interface PluginReadResult {
+  plugin: ParsedPlugin | null;
+  error: string;
+  record: PluginParamsRecord;
 }
