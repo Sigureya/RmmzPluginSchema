99a3490fd7ab672413af6fb773f0e1560beaaad6
Sigureya <sigureya1208@gmail.com>
2026-05-27 13:54:49 +0900

    テストを書きやすくするためにcontextを分けた

--- src/features/JSONPath/core/createPath/types/handlers.ts
+++ src/features/JSONPath/core/createPath/types/handlers.ts
@@ -1,20 +1,12 @@
 import type { StructPathError } from "./errorTypes";
 
+export interface JSONPathErrorContext {
+  pluginName: string;
+  commandName: string;
+  argName: string;
+}
+
 export interface BuildErrorHandlers<E> {
-  structPathError(
-    context: {
-      pluginName: string;
-      commandName: string;
-      argName: string;
-    },
-    error: StructPathError,
-  ): E;
-  compileJSONPathSchemaError(
-    context: {
-      pluginName: string;
-      commandName: string;
-      argName: string;
-    },
-    error: unknown,
-  ): E;
+  structPathError(context: JSONPathErrorContext, error: StructPathError): E;
+  compileJSONPathSchemaError(context: JSONPathErrorContext, error: unknown): E;
 }
