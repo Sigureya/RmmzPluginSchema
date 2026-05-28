c36cc688f8cc801d217abbe5d502252046d276af
Sigureya <sigureya1208@gmail.com>
2026-05-28 11:25:10 +0900

    旧実装を使ってないことが確定したので修正

--- src/features/JSONPath/core/index.ts
+++ src/features/JSONPath/core/index.ts
@@ -2,7 +2,7 @@ export * from "./command";
 export * from "./createPath";
 export type * from "./extractor/types";
 export * from "./extractor/value";
-export * from "./param";
+export * from "./param2";
 export * from "./schema";
 export * from "./types";
 export { extractAllPluginValues } from "./extractor/extractor";
