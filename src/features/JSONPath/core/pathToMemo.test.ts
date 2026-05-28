31402b0412f099908237ce17e60b4aea62cb1e21
Sigureya <sigureya1208@gmail.com>
2026-05-28 13:52:52 +0900

    テストだけ固定しておく

--- src/features/JSONPath/core/pathToMemo.test.ts
+++ src/features/JSONPath/core/pathToMemo.test.ts
@@ -509,6 +509,28 @@ describe("classroom path generation and value extraction", () => {
         `$["classroom"]["students"][*]["nicknames"][*]`,
       );
     });
+    test("例外が飛ぼうが全てのFactoryが動く", () => {
+      const mockFn = vi.fn((path) => {
+        throw new Error("Factory error");
+      });
+      compileJSONPathSchema(pathSchema, mockFn);
+      expect(mockFn).toBeCalledTimes(7);
+      expect(mockFn).toBeCalledWith('$["classroom"]["className"]');
+      expect(mockFn).toBeCalledWith('$["classroom"]["teacher"]["name","age"]');
+      expect(mockFn).toBeCalledWith(`$["classroom"]["teacher"]["items"][*]`);
+      expect(mockFn).toBeCalledWith(
+        `$["classroom"]["teacher"]["nicknames"][*]`,
+      );
+      expect(mockFn).toBeCalledWith(
+        '$["classroom"]["students"][*]["name","age"]',
+      );
+      expect(mockFn).toBeCalledWith(
+        `$["classroom"]["students"][*]["items"][*]`,
+      );
+      expect(mockFn).toBeCalledWith(
+        `$["classroom"]["students"][*]["nicknames"][*]`,
+      );
+    });
   });
   test("extracts all expected scalar values from ClassRoom object", () => {
     const paramObject = {
