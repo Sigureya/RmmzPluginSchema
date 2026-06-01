ea115f75d9c17b2172d502fa80d4c80d48d073ae
Sigureya <sigureya1208@gmail.com>
2026-06-01 14:28:06 +0900

    辞書データ。型名などは仮の状態

--- src/features/replace/handlers.ts
+++ src/features/replace/handlers.ts
@@ -1,4 +1,15 @@
-export interface ReplaceHandler {
-  findNewText: (path: string, oldValue: string) => string | undefined;
-  tansaStop(path: string): boolean;
+export interface ReplaceHandler2 {
+  isReplaceTarget(path: string): boolean;
+  findNewText(oldValue: string): string | undefined;
+}
+
+export interface TargetPath {
+  pluginName: string;
+  paramsPath: string[][];
+  commands: CommandXX[];
+}
+
+export interface CommandXX {
+  commandName: string;
+  argsPath: string[][];
 }
