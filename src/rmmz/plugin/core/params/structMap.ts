03c7cb705d7e1ea8754b841de60e4c8e9d333884
Sigureya <sigureya1208@gmail.com>
2026-06-01 20:55:19 +0900

    定義位置をまとめられそうなので移動。

--- src/rmmz/plugin/core/params/structMap.ts
+++ src/rmmz/plugin/core/params/structMap.ts
@@ -1,4 +1,5 @@
 import { classifyPluginParams } from "./classify";
+import { structDependencies } from "./structDependencies";
 import type {
   PluginArrayParamType,
   ClassifiedPluginParamsTyped,
@@ -7,7 +8,9 @@ import type {
   PluginStructSchemaArrayFiltered,
   PrimitiveParam,
   PluginScalarParam,
+  ParamKinds,
 } from "./types";
+import type { StructCollection } from "./types/structCollection";
 
 export const createClassifiedStructMap = <
   S extends PluginScalarParam,
@@ -32,4 +35,34 @@ export const createStructMap = (
       s.params.map((p) => p.attr),
     ]),
   );
+};
+
+export const collectStructsByKinds = (
+  structs: ReadonlyArray<PluginStructSchemaArray>,
+  kinds: ReadonlyArray<ParamKinds>,
+): StructCollection => {
+  const singleKinds: Set<ParamKinds> = new Set(kinds);
+  const arrayKinds = new Set(kinds.map((k): `${ParamKinds}[]` => `${k}[]`));
+  const scalaMatchedStructs = structs.filter((s) =>
+    isAnyAttributeKindMatched(s, singleKinds, arrayKinds),
+  );
+  const structMap = createStructMap(scalaMatchedStructs);
+  return {
+    targetArrayKinds: arrayKinds,
+    targetKinds: singleKinds,
+    matchedStructs: new Set(scalaMatchedStructs.map((s) => s.struct)),
+    nestedStructs: new Set(
+      structs.flatMap((s) => structDependencies(s.struct, structMap)),
+    ),
+  };
+};
+
+const isAnyAttributeKindMatched = (
+  struct: PluginStructSchemaArray,
+  single: ReadonlySet<string>,
+  array: ReadonlySet<string>,
+): boolean => {
+  return struct.params.some((p) => {
+    return single.has(p.attr.kind) || array.has(p.attr.kind);
+  });
 };
