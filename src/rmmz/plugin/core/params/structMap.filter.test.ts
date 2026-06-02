4b8361e2e22189f6a95febf6f72e8db5a3fdcd2b
Sigureya <sigureya1208@gmail.com>
2026-06-02 10:01:36 +0900

    特定の型のパラメータだけ残す関数。テストを簡略化するため、すべて消えるパターンだけ用意している

--- src/rmmz/plugin/core/params/structMap.filter.test.ts
+++ src/rmmz/plugin/core/params/structMap.filter.test.ts
@@ -30,7 +30,6 @@ const runTestCase = (testCase: TestCase) => {
     });
     test("returns expected struct names and structs", () => {
       const result = filterStructParamsByFn(testCase.schema, testCase.fn);
-      // expect(result.sttuctName).toEqual(new Set(testCase.expected.names));
       expect(result.structs).toEqual(testCase.expected.structs);
     });
   });
@@ -48,7 +47,52 @@ const testCases: TestCase[] = [
     },
   },
   {
-    caseName: "sss",
+    caseName: "all number",
+    schema: [
+      {
+        struct: "Vector2",
+        params: [
+          { name: "x", attr: { kind: "number", default: 0 } },
+          { name: "y", attr: { kind: "number", default: 0 } },
+        ],
+      },
+    ],
+    expected: {
+      structs: [
+        {
+          struct: "Vector2",
+          params: [
+            { name: "x", attr: { kind: "number", default: 0 } },
+            { name: "y", attr: { kind: "number", default: 0 } },
+          ],
+        },
+      ],
+    },
+    fn: (p) => p.kind === "number",
+  },
+  {
+    caseName: "mixed",
+    fn: (p) => p.kind === "string",
+    schema: [
+      {
+        struct: "Person",
+        params: [
+          { name: "name", attr: { kind: "string", default: "" } },
+          { name: "age", attr: { kind: "number", default: 0 } },
+        ],
+      },
+    ],
+    expected: {
+      structs: [
+        {
+          struct: "Person",
+          params: [{ name: "name", attr: { kind: "string", default: "" } }],
+        },
+      ],
+    },
+  },
+  {
+    caseName: "all string",
     schema: [
       {
         struct: "s1",
@@ -67,15 +111,40 @@ const testCases: TestCase[] = [
       },
     ],
     expected: {
-      structs: [
-        {
-          struct: "s3",
-          params: [
-            { name: "s1", attr: { kind: "struct", struct: "s1" } },
-            { name: "s2List", attr: { kind: "struct[]", struct: "s2" } },
-          ],
-        },
-      ],
+      structs: [],
+    },
+    fn: (p) => p.kind !== "string" && p.kind !== "string[]",
+  },
+
+  {
+    caseName: "sss",
+    schema: [
+      {
+        struct: "s1",
+        params: [{ name: "str", attr: { kind: "string", default: "" } }],
+      },
+      {
+        struct: "s2",
+        params: [{ name: "strList", attr: { kind: "string[]", default: [] } }],
+      },
+      {
+        struct: "s3",
+        params: [
+          { name: "s1", attr: { kind: "struct", struct: "s1" } },
+          { name: "s2List", attr: { kind: "struct[]", struct: "s2" } },
+        ],
+      },
+      {
+        struct: "s4",
+        params: [{ name: "s3", attr: { kind: "struct", struct: "s3" } }],
+      },
+      {
+        struct: "s5",
+        params: [{ name: "s4List", attr: { kind: "struct[]", struct: "s4" } }],
+      },
+    ],
+    expected: {
+      structs: [],
     },
     fn: (p) => p.kind !== "string" && p.kind !== "string[]",
   },
