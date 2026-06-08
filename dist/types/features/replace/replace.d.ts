df715414d715677c84dad6a4ea4169020417365b
Sigureya <sigureya1208@gmail.com>
2026-06-08 15:39:33 +0900

    パラメータ差し替え関数を微修正。型を色々と変えた

--- dist/types/features/replace/replace.d.ts
+++ dist/types/features/replace/replace.d.ts
@@ -1,6 +1,7 @@
-import { JSONValue } from '../../libs/jsonPath';
-import { PluginCommandData, PluginParamsObject, PluginParamsRecord } from '../../rmmz/plugin';
+import { DeepJSONValue, PluginCommandData, PluginParamsObject, PluginParamsRecord } from '../../rmmz/plugin';
 import { PluginCommandPathMap, PluginParamPathData } from './types';
+type DeepJSONObject = Record<string, DeepJSONValue>;
 export declare const replaceRuntimePluginCommand: (command: PluginCommandData, map: PluginCommandPathMap, replace: (value: string) => string | undefined) => PluginCommandData;
 export declare const replacePluginParams: (plugin: PluginParamsObject, map: PluginParamPathData, replace: (value: string) => string | undefined) => PluginParamsRecord;
-export declare const replacePluginValue: (params: Record<string, JSONValue>, paths: readonly (readonly string[])[], replace: (value: string) => string | undefined) => Record<string, JSONValue>;
+export declare const replacePluginValue: (params: DeepJSONObject, paths: readonly (readonly string[])[], replace: (value: string) => string | undefined) => DeepJSONObject;
+export {};
