b5767ed4cbfc54b097dac805403373ad2bf6f382
Sigureya <sigureya1208@gmail.com>
2026-06-09 22:32:21 +0900

    概ね動くのでビルド対象に追加

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./features";
 export * from "./fileio";
 export * from "./libs";
+export * from "./manifest";
 export * from "./pipleines";
 export * from "./pluginParam";
 export * from "./rmmz";
