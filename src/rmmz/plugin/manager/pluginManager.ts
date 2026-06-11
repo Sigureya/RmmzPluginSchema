478b70dffff33f600f484d7fdcd84e788fb72515
Sigureya <sigureya1208@gmail.com>
2026-06-11 16:17:18 +0900

    プラグインマネージャーの型だけ定義。これはプラグインコマンドを効率よく定義するためにある

---
+++ src/rmmz/plugin/manager/pluginManager.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @functional/no-return-void */
+export interface PluginManagerTemplate<
+  PluginName extends string,
+  CommandName extends string,
+  CommandArgs extends object,
+> {
+  registerCommand: (
+    name: PluginName,
+    commandName: CommandName,
+    args: CommandArgs,
+  ) => void;
+
+  callCommand(
+    self: unknown,
+    pluginName: PluginName,
+    commandName: CommandName,
+    args: CommandArgs,
+  ): void;
+}
