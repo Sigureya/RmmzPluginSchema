43b3427fddfdc2513a4ba69289c59d8f4690fc17
Sigureya <sigureya1208@gmail.com>
2026-06-12 15:17:40 +0900

    関数に間違いがあったので修正

--- dist/types/rmmz/plugin/manager/pluginManager.d.ts
+++ dist/types/rmmz/plugin/manager/pluginManager.d.ts
@@ -1,4 +1,4 @@
 export interface PluginManagerTemplate<PluginName extends string, CommandName extends string, CommandArgs extends object> {
-    registerCommand: (name: PluginName, commandName: CommandName, args: CommandArgs) => void;
+    registerCommand: (name: PluginName, commandName: CommandName, fn: (this: unknown, args: CommandArgs) => void) => void;
     callCommand(self: unknown, pluginName: PluginName, commandName: CommandName, args: CommandArgs): void;
 }
